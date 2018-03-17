5f1f51fd9b08a9d68aa28b04883e7e3e667a9bf4
Alexander Kluber <ajkluber@rice.edu>
2018-03-17 15:56:19 -0500

    fixes

--- solve_coeff.py
+++ solve_coeff.py
@@ -1,7 +1,9 @@
 import os
+import argparse
 import numpy as np
+import matplotlib
+matplotlib.use("Agg")
 import matplotlib.pyplot as plt
-from sklearn.cross_validation import KFold
 
 import mdtraj as md
 
@@ -9,56 +11,88 @@ import basis_library
 import util
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='.')
+    parser.add_argument('trajfile', help='Trajectory file.')
+    parser.add_argument('topfile', help='Trajectory file.')
+    parser.add_argument('--dt_frame', default=0.2, type=float, help='Timestep of one frame.')
+    parser.add_argument('--in_blocks', action="store_true", help='Trajectory file.')
+    parser.add_argument('--non_bond_gaussians', action="store_true", help='Trajectory file.')
+    parser.add_argument('--non_bond_wca', action="store_true", help='Trajectory file.')
+    parser.add_argument('--bonds', action="store_true", help='Trajectory file.')
+    parser.add_argument('--angles', action="store_true", help='Trajectory file.')
+
+    args = parser.parse_args()
+    trajfile = args.trajfile
+    topfile = args.topfile
+    in_blocks = args.in_blocks
+    dt_frame = args.dt_frame
+    bonds = args.bonds
+    angles = args.angles
+    non_bond_wca = args.non_bond_wca
+    non_bond_gaussians = args.non_bond_gaussians
+
     n_beads = 25 
     n_dim = 3*n_beads
     n_folds = 10
-    dt_frame = 0.2
+    #dt_frame = 0.2
 
     #topfile = "c25_min_ply_1.pdb"
     #trajfile = "c25_traj_ply_1.dcd"
-    topfile = "c25_min_1.pdb"
-    trajfile = "c25_traj_1.dcd"
+    #topfile = "c25_min_1.pdb"
+    #trajfile = "c25_traj_1.dcd"
+    #topfile = "c25_min_ply_1.pdb"
+    #trajfile = "c25_traj_ply_1.dcd"
+
+    if in_blocks:
+        n_blocks = 50
+    else:
+        n_blocks = False
 
     print "building basis function database..."
-    dU_funcs, dU_idxs, dU_d_arg, dU_dxi, dU_ck, scale_factors = basis_library.polymer_library(n_beads, gaussians=True)
+    dU_funcs, dU_idxs, dU_d_arg, dU_dxi, dU_ck, scale_factors = basis_library.polymer_library(n_beads, bonds=bonds, angles=angles, non_bond_wca=non_bond_wca, non_bond_gaussians=non_bond_gaussians)
     n_basis_deriv = len(dU_dxi)
     n_params = len(dU_funcs)
 
-    raise SystemExit
-
     all_s = [1, 2, 5, 10, 20, 50, 100, 500]
-    all_cv_scores = [] 
+    all_cv_scores = np.zeros(len(all_s), float)
     all_c_soln = []
     for z in range(len(all_s)):
         #for z in [0]:
         s_frames = all_s[z]
         s = dt_frame*s_frames
-
-        print "calculating trajectory derivatives..."
-        G, Y = util.calc_deriv_and_drift(trajfile, topfile, dU_funcs, dU_ck, dU_d_arg, dU_idxs, n_dim, n_params)
-
-        cv_score = 0
-        temp_c_soln = []
-        kf = KFold(Y.shape[0], n_folds=n_folds, shuffle=True)
-        for train_idxs, test_idxs in kf:
-            # cross-validation: solve regression on one part of data then test
-            # it on another. Helps measure predictability.
-            c_new = np.linalg.lstsq(G[train_idxs], Y[train_idxs])[0]
-            y_fit_new = np.dot(G, c_new)
-            cv_score += np.linalg.norm(Y[test_idxs] - y_fit_new[test_idxs], ord=2)
-            temp_c_soln.append(c_new)
-        all_c_soln.append(temp_c_soln)
-        cv_score /= float(n_folds)
-        all_cv_scores.append(cv_score)
-
+        c_solns, cv_score = util.solve_coefficients(trajfile, topfile, dU_funcs, dU_idxs, dU_d_arg, dU_dxi, dU_ck, s_frames, s, n_folds=10, n_blocks=n_blocks)
+        all_c_soln.append(c_solns)
+        all_cv_scores[z] = cv_score
+    all_c_soln = np.array(all_c_soln)
     all_s = np.array(all_s)
-    all_cv_scores = np.array(all_cv_scores)
 
     c_vs_s = []
     for n in range(n_params):
-        c_vs_s.append(scale_factors[n]*np.array([ np.array(all_c_soln[i])[:,n] for i in range(len(all_s)) ]))
+        if n in [0,1,2]:
+            c_vs_s.append(scale_factors[n]*np.array([ np.array(all_c_soln[i])[:,n] for i in range(all_c_soln.shape[0]) ]))
+        else:
+            c_vs_s.append(scale_factors[-1]*np.array([ np.array(all_c_soln[i])[:,n] for i in range(all_c_soln.shape[0]) ]))
 
     raise SystemExit
+    avg_c_vs_s = [] 
+    for i in range(10)
+        coeff = c_vs_s[i + 3]
+        avg_c_vs_s.append(np.mean(coeff, axis=1))
+
+    r = np.linspace(0.2, 1.5, 1000)
+    y = np.zeros(len(r), float)
+    for i in range(len(avg_c_vs_s)):
+        c_k = avg_c_vs_s[i][1]
+        y += c_k*gauss_funcs[i](r)
+
+
+    plt.figure()
+    for i in range(10):
+        coeff = c_vs_s[i + 3]
+        plt.errorbar(dt_frame*all_s[:len(coeff)], np.mean(coeff, axis=1), yerr=np.std(coeff, axis=1))
+    plt.ylabel(r"$c_k$", fontsize=20)
+    plt.savefig("coeff_gauss_vs_s.pdf")
+    plt.savefig("coeff_gauss_vs_s.png")
 
     ylabels = [r"$k_b$", r"$k_a$", r"$\epsilon$"]
 
--- util.py
+++ util.py
@@ -1,6 +1,8 @@
 import time
 import numpy as np
 
+from sklearn.cross_validation import KFold
+
 import mdtraj as md
 
 def get_n_frames(trajfile, topfile):
@@ -8,17 +10,16 @@ def get_n_frames(trajfile, topfile):
     for chunk in md.iterload(trajfile, top=topfile):
         n_frames_tot += chunk.n_frames
     n_frames_tot = float(n_frames_tot)
-    return n_frames_tot
+    n_dim = 3*chunk.xyz.shape[1]
+    return n_frames_tot, n_dim
 
-def calc_deriv_and_drift(trajfile, topfile, s_frames, dU_funcs, dU_ck, dU_d_arg, dU_idxs, n_dim):
+def calc_deriv_and_drift(trajfile, topfile, s_frames, dU_funcs, dU_ck, dU_d_arg, dU_idxs, n_dim, n_frames_tot):
 
     n_params = len(dU_funcs)
-    n_frames_tot = get_n_frames(trajfile, topfile)
     
     G = np.zeros((int(n_frames_tot)*n_dim, n_params), float)
     Y = np.zeros(int(n_frames_tot)*n_dim, float)
 
-    starttime = time.time()
     start_idx = 0
     total_n_iters = int(np.round(n_frames_tot/1000))
     iteration_idx = 0
@@ -46,10 +47,82 @@ def calc_deriv_and_drift(trajfile, topfile, s_frames, dU_funcs, dU_ck, dU_d_arg,
         iteration_idx += 1
         start_idx += ravel_size
 
+    G = G[:start_idx]
+    Y = Y[:start_idx]
+    return G, Y
+
+def solve_coefficients(trajfile, topfile, dU_funcs, dU_idxs, dU_d_arg, dU_dxi, dU_ck, s_frames, s, n_folds=10, n_blocks=False):
+    print "calculating trajectory derivatives..."
+    starttime = time.time()
+
+    n_params = len(dU_funcs)
+    n_frames_tot, n_dim = get_n_frames(trajfile, topfile)
+
+    if n_blocks:
+        chunksize = int(n_frames_tot)/n_blocks
+        total_n_iters = int(np.round(n_frames_tot/chunksize))
+        iteration_idx = 0
+        c_solns = []
+        all_cv_scores = []
+        for chunk in md.iterload(trajfile, top=topfile, chunk=chunksize):
+            # solve the problem on each chunk
+            G = np.zeros(((chunk.n_frames - s_frames)*n_dim, n_params), float)
+
+            if ((iteration_idx + 1) % 10) == 0:
+                print "  ({}/{})".format(iteration_idx + 1, total_n_iters)
+            xyz_flat = np.reshape(chunk.xyz, (chunk.n_frames, n_dim))
+
+            ravel_size = (chunk.n_frames - s_frames)*n_dim
+
+            # calculate forces
+            for i in range(len(dU_dxi)):
+                # derivative 
+                deriv_fun = dU_funcs[dU_ck[i]][dU_d_arg[i]]
+                deriv = deriv_fun(*xyz_flat[:,dU_idxs[i]].T)[:-s_frames]   # derivative k dxi_idx = dU_dxi[i]
+
+                # unraveled indices for xi 
+                xi_ravel_idxs = np.arange(dU_dxi[i], ravel_size, n_dim)
+                G[xi_ravel_idxs, dU_ck[i]] += deriv.ravel()
+
+            # calculate drift
+            Y = ((xyz_flat[s_frames:,:] - xyz_flat[:-s_frames,:])/s).ravel()
+
+            c_chunk = np.linalg.lstsq(G, Y)[0]
+            c_solns.append(c_chunk)
+
+            #chunk_cv_score = 0
+            #kf = KFold(Y.shape[0], n_folds=3, shuffle=True)
+            #for train_idxs, test_idxs in kf:
+            #    # cross-validation: solve regression on one part of data then test
+            #    # it on another. Helps measure predictability.
+            #    c_new = np.linalg.lstsq(G[train_idxs], Y[train_idxs])[0]
+            #    y_fit_new = np.dot(G, c_new)
+            #    chunk_cv_score += np.linalg.norm(Y[test_idxs] - y_fit_new[test_idxs], ord=2)
+            #chunk_cv_score /= float(3)
+            #all_cv_scores.append(chunk_cv_score)
+
+            iteration_idx += 1
+        #cv_score = np.mean(all_cv_scores)
+        cv_score = 0
+
+    else:
+        # calculate deriviative matrix on all data
+        G, Y = calc_deriv_and_drift(trajfile, topfile, dU_funcs, dU_ck, dU_d_arg, dU_idxs, n_dim, n_params, n_frames_tot)
+
+        cv_score = 0
+        c_solns = []
+        kf = KFold(Y.shape[0], n_folds=n_folds, shuffle=True)
+        for train_idxs, test_idxs in kf:
+            # cross-validation: solve regression on one part of data then test
+            # it on another. Helps measure predictability.
+            c_new = np.linalg.lstsq(G[train_idxs], Y[train_idxs])[0]
+            y_fit_new = np.dot(G, c_new)
+            cv_score += np.linalg.norm(Y[test_idxs] - y_fit_new[test_idxs], ord=2)
+            c_solns.append(c_new)
+        cv_score /= float(n_folds)
+
     stoptime = time.time()
     runmin = (stoptime - starttime)/60.
     print "calculation took: {} min".format(runmin)
 
-    G = G[:start_idx]
-    Y = Y[:start_idx]
-    return G, Y
+    return c_solns, cv_score 
