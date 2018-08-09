93c7165199de1564c4a2e7442ea1491dffa08d52
Alexander Kluber <ajkluber@rice.edu>
2018-08-09 15:26:25 -0500

    implemented block by block QR decomposition

--- util.py
+++ util.py
@@ -45,17 +45,18 @@ def calc_deriv_and_drift(trajfile, topfile, dU_funcs, dU_idxs, dU_d_arg, dU_dxi,
         if ((iteration_idx + 1) % 10) == 0:
             print "  ({}/{})".format(iteration_idx + 1, total_n_iters)
             sys.stdout.flush()
-        xyz_flat = np.reshape(chunk.xyz, (chunk.n_frames, n_dim))
+        if chunk.n_frames > s_frames:
+            xyz_flat = np.reshape(chunk.xyz, (chunk.n_frames, n_dim))
 
-        n_rows = (chunk.n_frames - s_frames)*n_dim
-        G = calc_derivative(xyz_flat, s, s_frames, n_dim, n_params, n_rows, dU_funcs, dU_idxs, dU_d_arg, dU_dxi, dU_ck, G=G)
+            n_rows = (chunk.n_frames - s_frames)*n_dim
+            G = calc_derivative(xyz_flat, s, s_frames, n_dim, n_params, n_rows, dU_funcs, dU_idxs, dU_d_arg, dU_dxi, dU_ck, G=G)
 
-        # calculate drift
-        Y_il = (xyz_flat[s_frames:,:] - xyz_flat[:-s_frames,:])/s
-        Y[start_idx:start_idx + n_rows] = Y_il.ravel()
+            # calculate drift
+            Y_il = (xyz_flat[s_frames:,:] - xyz_flat[:-s_frames,:])/s
+            Y[start_idx:start_idx + n_rows] = Y_il.ravel()
 
-        iteration_idx += 1
-        start_idx += n_rows
+            iteration_idx += 1
+            start_idx += n_rows
 
     G = G[:start_idx]
     Y = Y[:start_idx]
@@ -95,43 +96,44 @@ def solve_coefficients(trajfile, topfile, dU_funcs, dU_idxs, dU_d_arg, dU_dxi, d
         all_cv_scores = []
         for chunk in md.iterload(trajfile, top=topfile, chunk=chunksize):
             # solve the problem on each chunk
-            G = np.zeros(((chunk.n_frames - s_frames)*n_dim, n_params), float)
-
             if ((iteration_idx + 1) % 10) == 0:
                 print "  ({}/{})".format(iteration_idx + 1, total_n_iters)
                 sys.stdout.flush()
-            xyz_flat = np.reshape(chunk.xyz, (chunk.n_frames, n_dim))
 
-            n_rows = (chunk.n_frames - s_frames)*n_dim
+            if chunk.n_frames > s_frames:
+                G = np.zeros(((chunk.n_frames - s_frames)*n_dim, n_params), float)
+                xyz_flat = np.reshape(chunk.xyz, (chunk.n_frames, n_dim))
 
-            # calculate forces
-            for i in range(len(dU_dxi)):
-                # derivative 
-                deriv_fun = dU_funcs[dU_ck[i]][dU_d_arg[i]]
-                deriv = deriv_fun(*xyz_flat[:,dU_idxs[i]].T)[:-s_frames]   # derivative k dxi_idx = dU_dxi[i]
+                n_rows = (chunk.n_frames - s_frames)*n_dim
 
-                # unraveled indices for xi 
-                xi_ravel_idxs = np.arange(dU_dxi[i], n_rows, n_dim)
-                G[xi_ravel_idxs, dU_ck[i]] += deriv.ravel()
+                # calculate forces
+                for i in range(len(dU_dxi)):
+                    # derivative 
+                    deriv_fun = dU_funcs[dU_ck[i]][dU_d_arg[i]]
+                    deriv = deriv_fun(*xyz_flat[:,dU_idxs[i]].T)[:-s_frames]   # derivative k dxi_idx = dU_dxi[i]
 
-            # calculate drift
-            Y = ((xyz_flat[s_frames:,:] - xyz_flat[:-s_frames,:])/s).ravel()
+                    # unraveled indices for xi 
+                    xi_ravel_idxs = np.arange(dU_dxi[i], n_rows, n_dim)
+                    G[xi_ravel_idxs, dU_ck[i]] += deriv.ravel()
 
-            c_chunk = np.linalg.lstsq(G, Y)[0]
-            c_solns.append(c_chunk)
+                # calculate drift
+                Y = ((xyz_flat[s_frames:,:] - xyz_flat[:-s_frames,:])/s).ravel()
 
-            #chunk_cv_score = 0
-            #kf = KFold(Y.shape[0], n_folds=3, shuffle=True)
-            #for train_idxs, test_idxs in kf:
-            #    # cross-validation: solve regression on one part of data then test
-            #    # it on another. Helps measure predictability.
-            #    c_new = np.linalg.lstsq(G[train_idxs], Y[train_idxs])[0]
-            #    y_fit_new = np.dot(G, c_new)
-            #    chunk_cv_score += np.linalg.norm(Y[test_idxs] - y_fit_new[test_idxs], ord=2)
-            #chunk_cv_score /= float(3)
-            #all_cv_scores.append(chunk_cv_score)
+                c_chunk = np.linalg.lstsq(G, Y)[0]
+                c_solns.append(c_chunk)
 
-            iteration_idx += 1
+                #chunk_cv_score = 0
+                #kf = KFold(Y.shape[0], n_folds=3, shuffle=True)
+                #for train_idxs, test_idxs in kf:
+                #    # cross-validation: solve regression on one part of data then test
+                #    # it on another. Helps measure predictability.
+                #    c_new = np.linalg.lstsq(G[train_idxs], Y[train_idxs])[0]
+                #    y_fit_new = np.dot(G, c_new)
+                #    chunk_cv_score += np.linalg.norm(Y[test_idxs] - y_fit_new[test_idxs], ord=2)
+                #chunk_cv_score /= float(3)
+                #all_cv_scores.append(chunk_cv_score)
+
+                iteration_idx += 1
         #cv_score = np.mean(all_cv_scores)
         cv_score = 0
     elif method == "qr":
@@ -149,31 +151,32 @@ def solve_coefficients(trajfile, topfile, dU_funcs, dU_idxs, dU_d_arg, dU_dxi, d
                 print "  ({}/{})".format(iteration_idx + 1, total_n_iters)
                 sys.stdout.flush()
 
-            n_rows = (chunk.n_frames - s_frames)*n_dim
-            xyz_flat = np.reshape(chunk.xyz, (chunk.n_frames, n_dim))
-            G = calc_derivative(xyz_flat, s, s_frames, n_dim, n_params, n_rows, dU_funcs, dU_idxs, dU_d_arg, dU_dxi, dU_ck)
+            if chunk.n_frames > s_frames:
+                n_rows = (chunk.n_frames - s_frames)*n_dim
+                xyz_flat = np.reshape(chunk.xyz, (chunk.n_frames, n_dim))
+                G = calc_derivative(xyz_flat, s, s_frames, n_dim, n_params, n_rows, dU_funcs, dU_idxs, dU_d_arg, dU_dxi, dU_ck)
 
-            # calculate drift
-            Y = ((xyz_flat[s_frames:,:] - xyz_flat[:-s_frames,:])/s).ravel()
+                # calculate drift
+                Y = ((xyz_flat[s_frames:,:] - xyz_flat[:-s_frames,:])/s).ravel()
 
-            if iteration_idx == 0:
-                Q, R = scl.qr(G, mode="economic")
+                if iteration_idx == 0:
+                    Q, R = scl.qr(G, mode="economic")
 
-                X = np.zeros((n_params + max_rows, n_params), float)
-                b = np.zeros(n_params + max_rows, float)
+                    X = np.zeros((n_params + max_rows, n_params), float)
+                    b = np.zeros(n_params + max_rows, float)
 
-                X[:n_params,:] = R[:n_params,:].copy()
-                b[:n_params] = np.dot(Q.T, Y)
-            else:
-                X[n_params:n_params + n_rows,:] = G
-                b[n_params:n_params + n_rows] = Y
+                    X[:n_params,:] = R[:n_params,:].copy()
+                    b[:n_params] = np.dot(Q.T, Y)
+                else:
+                    X[n_params:n_params + n_rows,:] = G
+                    b[n_params:n_params + n_rows] = Y
 
-                Qk, Rk = scl.qr(X[:n_rows,:], mode="economic")
+                    Qk, Rk = scl.qr(X[:n_rows,:], mode="economic")
 
-                X[:n_params,:] = Rk
-                b[:n_params] = np.dot(Qk.T, b[:n_rows])
-                
-            iteration_idx += 1
+                    X[:n_params,:] = Rk
+                    b[:n_params] = np.dot(Qk.T, b[:n_rows])
+                    
+                iteration_idx += 1
         final_R = X[:n_params,:]
         final_b = b[:n_params]
         c_soln = scl.solve(final_R, final_b)
