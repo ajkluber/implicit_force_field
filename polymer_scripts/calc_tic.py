3dcce2eb7f18d1b1b694b5999bdcba2f60bbe841
Alexander Kluber <ajkluber@rice.edu>
2018-08-28 13:41:12 -0500

    save MSM implied timescale plot

--- polymer_scripts/calc_tic.py
+++ polymer_scripts/calc_tic.py
@@ -11,57 +11,21 @@ def tanh_contact(traj, pairs, r0, widths):
     r = md.compute_distances(traj, pairs)
     return 0.5*(np.tanh((r0 - r)/widths) + 1)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("name")
-    #parser.add_argument("subdir", type=str)
-    args = parser.parse_args()
+def rg_feature(traj):
+    return md.compute_rg(traj).astype(np.float32).reshape(-1, 1)
 
-    name = args.name
-    #subidr = args.subdir
+def local_density_feature(traj, pairs, r0, widths):
 
-    display = False
+    rho_i = np.zeros((traj.n_frames, traj.n_atoms), np.float32)
+    for i in range(len(pairs)):
+        r = md.compute_distances(traj, pairs[i])
+        rho_i[:,i] = np.sum(0.5*(np.tanh((r0 - r)/widths) + 1))
+    #r = md.compute_distances(traj, pairs)
+    #rho_i = np.sum(0.5*(np.tanh((r0 - r)/widths) + 1), axis=1).astype(np.float32).reshape(-1,1)
+    return rho_i
 
-    if display:
-        import pyemma.plots as mplt
-    else:
-        import matplotlib
-        matplotlib.use("Agg")
-        import matplotlib.pyplot as plt
-        import pyemma.plots as mplt
+def tica_stuff():
 
-    import pyemma.coordinates as coor
-    import pyemma.msm as msm
-    from pyemma.coordinates.data.featurization.misc import CustomFeature
-
-
-    # get trajectory names
-    trajnames = glob.glob("run_*/" + name + "_traj_cent_*.dcd")
-    topfile = glob.glob("run_*/" + name + "_min_cent.pdb")[0]
-
-    # get indices for dihedral angles and pairwise distances
-    feat = coor.featurizer(topfile)
-    ply_idxs = feat.topology.select("name PL")
-
-    dih_idxs = np.array([[ply_idxs[i], ply_idxs[i + 1], ply_idxs[i + 2], ply_idxs[i + 3]] for i in range(len(ply_idxs) - 4) ])
-    pair_idxs = []
-    for i in range(len(ply_idxs) - 1):
-        for j in range(i + 3, len(ply_idxs)):
-            pair_idxs.append([ply_idxs[i], ply_idxs[j]])
-    pair_idxs = np.array(pair_idxs)
-
-    r0 = 0.4
-    widths = 0.1
-
-    # add dihedrals
-    #feat.add_custom_feature(CustomFeature(tanh_contact, len(pair_idxs), fun_args=(pair_idxs, r0, widths)))
-    feat.add_dihedrals(dih_idxs, cossin=True)
-    feat.add_distances(pair_idxs)
-    #feature_info = {'pairs':pair_idxs, 'r0':r0, 'widths':widths, 'dim':len(pair_idxs)}
-
-    reader = coor.source(trajnames, features=feat)
-
-    keep_dims = 20
     #lagtimes = [1, 5, 10, 100, 500, 1000]
     lagtimes = [1, 2, 5, 8, 10, 20, 50]
     ti = []
@@ -109,22 +73,106 @@ if __name__ == "__main__":
     plt.ylabel("Cum. Var.")
     plt.savefig("cumvar_vs_index.pdf")
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("name")
+    #parser.add_argument("subdir", type=str)
+    args = parser.parse_args()
 
-    tica = coor.tica(lag=10, stride=1, dim=10)
-    cluster = coor.cluster_kmeans(k=500)
-    coor.pipeline([reader, tica, cluster])
-    M = msm.estimate_markov_model(cluster.dtrajs, 1)
-    
+    name = args.name
+    #subidr = args.subdir
+
+    display = False
+
+    if display:
+        import pyemma.plots as mplt
+    else:
+        import matplotlib
+        matplotlib.use("Agg")
+        import matplotlib.pyplot as plt
+        import pyemma.plots as mplt
+
+    import pyemma.coordinates as coor
+    import pyemma.msm as msm
+    from pyemma.coordinates.data.featurization.misc import CustomFeature
+
+    #feature_set = ["dih", "invdists"]
+    feature_set = ["dih", "tanh", "rho"]
+    #feature_set = ["dih", "rho"]
+    #feature_set = ["dih", "invdists", "rg"]
+    #feature_set = ["dih", "dists"]
+    #feature_set = ["dih", "dists", "rg"]
+    msm_savedir = "msm_" + "_".join(feature_set)
+
+    if not os.path.exists(msm_savedir):
+        os.mkdir(msm_savedir)
+
+    # get trajectory names
+    trajnames = glob.glob("run_*/" + name + "_traj_cent_*.dcd")
+    topfile = glob.glob("run_*/" + name + "_min_cent.pdb")[0]
+
+    # get indices for dihedral angles and pairwise distances
+    feat = coor.featurizer(topfile)
+    ply_idxs = feat.topology.select("name PL")
+
+    dih_idxs = np.array([[ply_idxs[i], ply_idxs[i + 1], ply_idxs[i + 2], ply_idxs[i + 3]] for i in range(len(ply_idxs) - 4) ])
+    pair_idxs = []
+    for i in range(len(ply_idxs) - 1):
+        for j in range(i + 3, len(ply_idxs)):
+            pair_idxs.append([ply_idxs[i], ply_idxs[j]])
+    pair_idxs = np.array(pair_idxs)
+
+    all_pair_idxs = []
+    for i in range(len(ply_idxs)):
+        this_residue = []
+        for j in range(len(ply_idxs)):
+            if i < j:
+                this_residue.append([ply_idxs[i], ply_idxs[j]])
+            elif j < i:
+                this_residue.append([ply_idxs[j], ply_idxs[i]])
+            else:
+                pass
+        all_pair_idxs.append(this_residue)
+    all_pair_idxs = np.array(all_pair_idxs)
+
+    r0 = 0.4
+    widths = 0.1
+
+    # add dihedrals
+    if "dih" in feature_set:
+        feat.add_dihedrals(dih_idxs, cossin=True)
+    if "tanh" in feature_set:
+        feat.add_custom_feature(CustomFeature(tanh_contact, len(pair_idxs), fun_args=(pair_idxs, r0, widths)))
+    if "dists" in feature_set:
+        feat.add_distances(pair_idxs)
+    if "invdists" in feature_set:
+        feat.add_inverse_distances(pair_idxs)
+    if "rho":
+        feat.add_custom_feature(CustomFeature(local_density_feature, len(all_pair_idxs), fun_args=(all_pair_idxs, r0, widths)))
+    if "rg" in feature_set:
+        feat.add_custom_feature(CustomFeature(rg_feature, 1))
+
+    #feature_info = {'pairs':pair_idxs, 'r0':r0, 'widths':widths, 'dim':len(pair_idxs)}
+
+    reader = coor.source(trajnames, features=feat)
+
+    tica_lag = 10
+    keep_dims = 10
+    n_clusters = 100
     msm_lags = [1, 10, 20, 50, 100, 200]
+
+    tica = coor.tica(lag=tica_lag, stride=1, dim=keep_dims)
+    cluster = coor.cluster_kmeans(k=n_clusters)
+    coor.pipeline([reader, tica, cluster])
     its = msm.its(cluster.dtrajs, lags=msm_lags)
+
     plt.figure()
     mplt.plot_implied_timescales(its, ylog=False)
-    plt.savefig("msm_its.pdf")
+    plt.title(msm_savedir)
+    plt.savefig(msm_savedir + "/its_vs_lag.pdf")
 
-
-
-    plt.figure()
-    plt.plot(np.arange(1,21), M.timescales()[:20], 'o')
-    ymin, ymax = plt.ylim()
-    plt.ylim(0, ymax)
-    plt.savefig("msm_ti.pdf")
+    #plt.figure()
+    #plt.plot(np.arange(1,21), M.timescales()[:20], 'o')
+    #ymin, ymax = plt.ylim()
+    #plt.ylim(0, ymax)
+    #plt.savefig("msm_ti.pdf")
