e0a2f9c6d051d4bec32edebe97a7e8c5bd3fd01c
Alexander Kluber <ajkluber@rice.edu>
2018-10-18 18:14:45 -0500

    testing polymer model basis and test functions

--- basis_library.py
+++ basis_library.py
@@ -128,6 +128,10 @@ class FunctionLibrary(object):
         self.U_coord_idxs[fixd].append(temp_U_coord_idxs)
         self.dU_funcs[fixd].append(temp_dU_funcs)
 
+    @property
+    def n_params(self):
+        return len(self.U_funcs[1])
+
     def calculate_potential_terms(self, traj):
 
         xyz_flat = np.reshape(traj.xyz, (traj.n_frames, self.n_dof))
@@ -212,12 +216,11 @@ class FunctionLibrary(object):
 
         xyz_flat = np.reshape(traj.xyz, (traj.n_frames, self.n_dof))
         n_rows = (traj.n_frames - s_frames)*self.n_dof
-        n_params = len(self.U_funcs[1])
 
         if G is None:
-            G = np.zeros((n_rows, n_params), float)
+            G = np.zeros((n_rows, self.n_params), float)
 
-        for i in range(n_params): 
+        for i in range(self.n_params): 
             # parameter i corresponds to functional form i
             # coords assigned to functional form i
             coord_idxs = self.U_coord_idxs[1][i]    # I_r
@@ -263,6 +266,10 @@ class PolymerModel(FunctionLibrary):
         self.df_funcs = []          # first derivative of each form wrt each of its arguments, lambdified
         self.d2f_funcs = []         # second derivative of each form wrt each of its arguments, lambdified
 
+    @property
+    def n_test_funcs(self):
+        return np.sum([ len(self.f_coord_idxs[i]) for i in range(len(self.f_funcs)) ])
+
     def _add_test_functions(self, f_sym, f_lamb, temp_f_coord_idxs, temp_df_funcs, temp_d2f_funcs):
 
         self.f_sym.append(f_sym)
@@ -291,7 +298,9 @@ class PolymerModel(FunctionLibrary):
             coord_idxs.append(xi_idxs)
         return coord_idxs
 
-
+    ##########################################################3
+    # ASSIGN POTENTIAL FUNCTIONS
+    ##########################################################3
     def _assign_harmonic_bonds(self, r0_nm, scale_factor=1, fixed=False):
         """Assign harmonic bond interactions
         
@@ -392,12 +401,12 @@ class PolymerModel(FunctionLibrary):
 
         self._add_assignments(fixed, U_sym, U_lamb, scale_factor, temp_U_coord_idxs, temp_dU_funcs)
 
-    def _assign_pairwise_gaussians(self, r0_nm, w_nm, scaling=1, fixed=False, bond_cutoff=3):
+    def _assign_pairwise_gaussians(self, r0_nm, w_nm, scale_factor=1, fixed=False, bond_cutoff=3):
         """Assign a Gaussian well a each position"""
         
         for m in range(len(r0_nm)):
             # gaussian well at position r0_nm[m]
-            U_sym = -scaling_factor*sympy.exp(-self.one_half*((self.r12_sym - r0_nm[m])/w_nm[m])**2)
+            U_sym = -scale_factor*sympy.exp(-self.one_half*((self.r12_sym - r0_nm[m])/w_nm[m])**2)
             U_lamb = sympy.lambdify(self.rij_args, U_sym, modules="numpy")
 
             temp_U_coord_idxs = self._generate_pairwise_idxs(bond_cutoff=bond_cutoff)
@@ -410,8 +419,9 @@ class PolymerModel(FunctionLibrary):
 
             self._add_assignments(fixed, U_sym, U_lamb, scale_factor, temp_U_coord_idxs, temp_dU_funcs)
 
+
     ##########################################################3
-    ## TEST FUNCTIONS BELOW
+    # ASSIGN TEST FUNCTIONS
     ##########################################################3
     def _assign_bond_funcs(self, r0_nm, w_nm, coeff=1):
         """Assign harmonic bond interactions
@@ -529,19 +539,126 @@ class PolymerModel(FunctionLibrary):
 
             self._add_test_functions(f_sym, f_lamb, temp_f_coord_idxs, temp_df_funcs, temp_d2f_funcs)
             
-    def calculate_test_func_gradient(self, traj):
 
-        n_test_funcs = np.sum([ len(self.f_coord_idxs[i]) for i in range(len(self.f_funcs)) ])
+    ##################################################
+    # EVALUATE POTENTIAL ON TRAJECTORY
+    ##################################################
+    def calculate_U0_gradient(self, traj):
+        """Gradient of fixed potential terms
+        
+        Parameters
+        ----------
+        traj : mdtraj.Trajectory
+            
+        Returns
+        -------
+        grad_U0 : np.ndarray
+            Matrix of gradients
+        """
+
+        xyz_flat = np.reshape(traj.xyz, (traj.n_frames, self.n_dof))
+
+        grad_U0 = np.zeros((traj.n_frames, self.n_dof), float)
+
+        for i in range(len(self.U_funcs[0])): 
+            # parameter i corresponds to functional form i
+            # coords assigned to functional form i
+            coord_idxs = self.U_coord_idxs[0][i]    # I_r
+
+            for j in range(len(self.dU_funcs[0][i])):
+                # derivative wrt argument j
+                d_func = self.dU_funcs[0][i][j]
+                
+                for n in range(len(coord_idxs)):
+                    # coordinates assigned to this derivative
+                    deriv = d_func(*xyz_flat[:,coord_idxs[n]].T)
+
+                    # derivative is wrt to coordinate index dxi
+                    dxi = coord_idxs[n][j]
+
+                    # force on each coordinate is separated by associated
+                    # parameter
+                    grad_U0[:, dxi] += deriv
+        return grad_U0
+
+    def calculate_U_gradient(self, traj):
+        """Gradient of potential form associated with each parameter
+        
+        Parameters
+        ----------
+        traj : mdtraj.Trajectory
+        
+        Returns
+        -------
+        grad_U : np.ndarray
+            Matrix of gradients
+        """
+
+        xyz_flat = np.reshape(traj.xyz, (traj.n_frames, self.n_dof))
+
+        grad_U = np.zeros((traj.n_frames, self.n_dof, self.n_params), float)
+
+        for i in range(self.n_params): 
+            # parameter i corresponds to functional form i
+            # coords assigned to functional form i
+            coord_idxs = self.U_coord_idxs[1][i]    # I_r
+
+            for j in range(len(self.dU_funcs[1][i])):
+                # derivative wrt argument j
+                d_func = self.dU_funcs[1][i][j]
+                
+                for n in range(len(coord_idxs)):
+                    # coordinates assigned to this derivative
+                    deriv = d_func(*xyz_flat[:,coord_idxs[n]].T)
+
+                    # derivative is wrt to coordinate index dxi
+                    dxi = coord_idxs[n][j]
+
+                    # force on each coordinate is separated by associated
+                    # parameter
+                    grad_U[:, dxi, i] += deriv
+        return grad_U
+
+    #########################################################
+    # EVALUATE TEST FUNCTIONS ON TRAJECTORY
+    #########################################################
+    def calculate_test_funcs(self, traj):
+        """Test functions"""
+
+        #n_test_funcs = np.sum([ len(self.f_coord_idxs[i]) for i in range(len(self.f_funcs)) ])
+
+        xyz_flat = np.reshape(traj.xyz, (traj.n_frames, self.n_dof))
+
+        test_fj = np.zeros((traj.n_frames, self.n_test_funcs), float)
+        start_idx = 0
+        for j in range(len(self.f_funcs)):
+            # test function form j
+            f_j_func = self.f_funcs[j]
+
+            for n in range(len(self.f_coord_idxs[j])):
+                # each coordinate assignment is a different test function
+                xi_idxs = self.f_coord_idxs[j][n]
+                test_fj[:, start_idx + n] = f_j_func(*xyz_flat[:,xi_idxs].T)
+            start_idx += len(self.f_coord_idxs[j])
+
+        return test_fj
+            
+    def calculate_test_func_gradient(self, traj):
+        """Gradient of test functions"""
+
+        #n_test_funcs = np.sum([ len(self.f_coord_idxs[i]) for i in range(len(self.f_funcs)) ])
 
         xyz_flat = np.reshape(traj.xyz, (traj.n_frames, self.n_dof))
 
         grad_fj = np.zeros((traj.n_frames, self.n_dof, self.n_test_funcs), float)
+        
         start_idx = 0
         for j in range(len(self.f_funcs)):
             for n in range(len(self.f_coord_idxs[j])):
                 # coordinates assigned to function j
                 xi_idxs = self.f_coord_idxs[j][n]
 
+                # innermost loop calculates components of grad f_j({x}_n)
                 for i in range(len(self.df_funcs[j])):
                     # first derivative wrt argument i
                     dxi = xi_idxs[i] 
@@ -554,12 +671,15 @@ class PolymerModel(FunctionLibrary):
         return grad_fj
 
     def calculate_test_func_laplacian(self, traj):
+        """Laplacian of test functions"""
 
-        n_test_funcs = len(self.f_funcs)
+        #n_test_funcs = np.sum([ len(self.f_coord_idxs[i]) for i in range(len(self.f_funcs)) ])
 
         xyz_flat = np.reshape(traj.xyz, (traj.n_frames, self.n_dof))
 
         Lap_fj = np.zeros((traj.n_frames, self.n_test_funcs), float)
+
+        start_idx = 0
         for j in range(len(self.f_funcs)):
             for n in range(len(self.f_coord_idxs[j])):
                 # coordinates assigned to function j
@@ -568,74 +688,11 @@ class PolymerModel(FunctionLibrary):
                 for i in range(len(self.d2f_funcs[j])):
                     # add the double derivative wrt argument i
                     d2f_j_func = self.d2f_funcs[j][i]
-                    Lap_fj[:,j] += d2f_j_func(*xyz_flat[:,xi_idxs].T)
+                    Lap_fj[:, start_idx + n] += d2f_j_func(*xyz_flat[:,xi_idxs].T)
+
+            start_idx += len(self.f_coord_idxs[j])
 
         return Lap_fj
-
-    def calculate_test_func(self, traj):
-
-        n_test_funcs = len(self.f_funcs)
-
-        xyz_flat = np.reshape(traj.xyz, (traj.n_frames, self.n_dof))
-
-        fj = np.zeros((traj.n_frames, self.n_test_funcs), float)
-        for j in range(len(self.f_funcs)):
-            f_j_func = self.f_funcs[j]
-            for n in range(len(self.f_coord_idxs[j])):
-                # coordinates assigned to function j
-                xi_idxs = self.f_coord_idxs[j][n]
-                fj[:,j] += d2f_j_func(*xyz_flat[:,xi_idxs].T)
-
-        return fj
-
-    def calculate_grad_ur(self, traj):
-        """Calculate total force due to each parameter along each dimension
-        
-        Parameters
-        ----------
-        traj : mdtraj.Trajectory
-        
-        s_frames : int
-            Number of frames to . s_frames > 0 when using the Kramers-Moyal
-            relation.
-            
-        Returns
-        -------
-        G : np.ndarray
-            Matrix of 
-        """
-
-        xyz_flat = np.reshape(traj.xyz, (traj.n_frames, self.n_dof))
-        n_params = len(self.U_funcs[1])
-
-        G = np.zeros((traj.n_frames, self.n_dof, n_params), float)
-
-        for i in range(n_params): 
-            # parameter i corresponds to functional form i
-            # coords assigned to functional form i
-            coord_idxs = self.U_coord_idxs[1][i]    # I_r
-
-            for j in range(len(self.dU_funcs[1][i])):
-                # derivative wrt argument j
-                d_func = self.dU_funcs[1][i][j]
-                
-                for n in range(len(coord_idxs)):
-                    # coordinates assigned to this derivative
-                    deriv = -d_func(*xyz_flat[:,coord_idxs[n]].T)
-
-                    # derivative is wrt to coordinate index dxi
-                    dxi = coord_idxs[n][j]
-
-                    # force on each coordinate is separated by associated
-                    # parameter
-                    G[:, dxi, i] += deriv.ravel()
-        return G
-
-    def calculate_eigenpair_matrix(self):
-        pass
-
-    def calculate_eigenpair_vector(self):
-        pass
 
 def polymer_library(n_beads, bonds=True, angles=True, non_bond_wca=True, non_bond_gaussians=True):
     # Soon to be deprecated oct 2018
