22a68baa5e23e14b04cd401ff5139a134459928e
Alexander Kluber <ajkluber@rice.edu>
2019-01-09 18:22:45 -0600

    frame assignments to help create training and test sets for cross validation

--- basis_library.py
+++ basis_library.py
@@ -329,6 +329,8 @@ class FunctionLibrary(object):
 
 
 class OneDimensionalModel(FunctionLibrary):
+    # TODO: Simpler support for low-dimension systems
+
     def __init__(self, n_atoms, periodic_box_dims=[]):
         """One-dimensional model
         
@@ -987,7 +989,7 @@ class PolymerModel(FunctionLibrary):
             # the gradient requires chain rule 
             Jac = self._cv_cartesian_Jacobian(xyz_traj)
 
-            grad_cv_U1 = np.zeros((xyz.shape[0], self.n_cv_dim, self.n_params), float)
+            grad_cv_U1 = np.zeros((xyz_traj.shape[0], self.n_cv_dim, self.n_params), float)
             for i in range(self.n_params): 
                 for j in range(len(self.cv_dU_funcs[i])):
                     # derivative wrt argument j
@@ -997,7 +999,7 @@ class PolymerModel(FunctionLibrary):
             grad_x_U1 = np.einsum("tnd,tnr->tdr", Jac, grad_cv_U1)
         else:
             # gradient with respect to Cartesian coordinates, directly. 
-            grad_x_U1 = np.zeros((xyz.shape[0], self.n_dof, self.n_params), float)
+            grad_x_U1 = np.zeros((xyz_traj.shape[0], self.n_dof, self.n_params), float)
 
             for i in range(self.n_params): 
                 # parameter i corresponds to functional form i
@@ -1201,7 +1203,7 @@ class PolymerModel(FunctionLibrary):
     ###############################################################
     # CALCULATE MATRICES FOR EIGENPAIR METHOD
     ###############################################################
-    def setup_eigenpair(self, trajnames, topfile, psinames, ti_file, M=1, cv_names=[]):
+    def setup_eigenpair(self, trajnames, topfile, psinames, ti_file, M=1, cv_names=[], verbose=False, set_assignment=None):
         """Calculate eigenpair matrices
        
         Parameters
@@ -1228,12 +1230,21 @@ class PolymerModel(FunctionLibrary):
         """
         # TODO: allow collective variable to be different than eigenvector
 
+        if not set_assignment is None:
+            n_sets = np.max([ np.max(x) for x in set_assignment]) + 1
+        else:
+            n_sets = 1
+
         R = self.n_params
         P = self.n_test_funcs
 
-        X = np.zeros((P, R + 1), float)
-        d = np.zeros(P, float)
-        D2 = np.zeros((R + 1, R + 1), float)    # TODO: high-dimensional smoothness 
+        # if constant diff coeff
+        if self.constant_diff:
+            X = np.zeros((n_sets, P, R + 1), float)
+            d = np.zeros((n_sets, P), float)
+            D2 = np.zeros((n_sets, R + 1, R + 1), float)    # TODO: high-dimensional smoothness 
+        else:
+            raise NotImplementedError("Only constant diffusion coefficient is supported.")
 
         if self.using_cv and not self.cv_defined:
             raise ValueError("Collective variables are not defined!")
@@ -1243,11 +1254,14 @@ class PolymerModel(FunctionLibrary):
 
         kappa = 1./np.load(ti_file)[:M]
 
-        print("calculating eigenpair matrices...")
+        if verbose:
+            print("calculating eigenpair matrices...")
         N_prev = 0
+        N_prev_set = np.zeros(n_sets, int)
         for n in range(len(trajnames)):
-            print("  traj: " + str(n+1))
-            sys.stdout.flush()
+            if verbose:
+                print("  traj: {}/{}".format(n + 1, len(trajnames)))
+                sys.stdout.flush()
             # load eigenvectors
             psi_traj = np.array([ np.load(temp_psiname) for temp_psiname in psinames[n] ]).T
 
@@ -1258,17 +1272,17 @@ class PolymerModel(FunctionLibrary):
 
             start_idx = 0
             for chunk in md.iterload(trajnames[n], top=topfile, chunk=1000):
-                N_curr = chunk.n_frames
+                N_chunk = chunk.n_frames
 
-                psi_chunk = psi_traj[start_idx:start_idx + N_curr,:]
+                psi_chunk = psi_traj[start_idx:start_idx + N_chunk,:]
 
                 if cv_traj is None:
                     cv_chunk = self.calculate_cv(chunk)
                 else:
-                    cv_chunk = cv_traj[start_idx:start_idx + N_curr,:]
+                    cv_chunk = cv_traj[start_idx:start_idx + N_chunk,:]
 
                 # cartesian coordinates unraveled
-                xyz_traj = np.reshape(chunk.xyz, (N_curr, self.n_dof))
+                xyz_traj = np.reshape(chunk.xyz, (N_chunk, self.n_dof))
 
                 # calculate gradient of fixed and parametric potential terms
                 grad_U1 = self.gradient_U1(xyz_traj, cv_chunk)
@@ -1277,32 +1291,125 @@ class PolymerModel(FunctionLibrary):
                 test_f = self.test_functions(xyz_traj, cv_chunk)
                 grad_f, Lap_f = self.test_funcs_gradient_and_laplacian(xyz_traj, cv_chunk)
 
-                # very useful einstein summation function to calculate
-                # dot products with eigenvectors
-                curr_X1 = np.einsum("tm,tdr,tdp->mpr", Psi, -grad_U1, grad_f).reshape((M*P, R))
-                curr_X2 = np.einsum("m,tm,tp->mp", kappa, Psi, test_f).reshape(M*P)
-
-                curr_d = (-1./self.beta)*np.einsum("tm,tp->mp", Psi, Lap_f).reshape(M*P)
-
                 if self.using_U0:
-                    grad_U0 = self.gradient_U0(xyz_traj, Psi)
-                    curr_d += np.einsum("tm,td,tdp->mp", Psi, grad_U0, grad_f).reshape(M*P)
+                    grad_U0 = self.gradient_U0(xyz_traj, psi_chunk)
 
                 if self.using_D2:
                     #TODO
                     pass
 
+                if n_sets == 1: 
+                    # dot products with eigenvectors
+                    curr_X1 = np.einsum("tm,tdr,tdp->mpr", psi_chunk, -grad_U1, grad_f).reshape((M*P, R))
+                    curr_X2 = np.einsum("m,tm,tp->mp", kappa, psi_chunk, test_f).reshape(M*P)
+
+                    curr_d = (-1./self.beta)*np.einsum("tm,tp->mp", psi_chunk, Lap_f).reshape(M*P)
+
+                    if self.using_U0:
+                        curr_d += np.einsum("tm,td,tdp->mp", psi_chunk, grad_U0, grad_f).reshape(M*P)
+
+                    # running average to reduce numerical error
+                    X[0,:,:-1] = (curr_X1 + N_prev*X[0,:,:-1])/(N_prev + N_chunk)
+                    X[0,:,-1] = (curr_X2 + N_prev*X[0,:,-1])/(N_prev + N_chunk)
+                    d[0,:] = (curr_d + N_prev*d[0,:])/(N_prev + N_chunk)
+
+                    start_idx += N_chunk
+                    N_prev += N_chunk
+                else:
+                    for k in range(n_sets):   
+                        frames_in_this_set = set_assignment[n][start_idx:start_idx + N_chunk] == k
+
+                        if np.any(frames_in_this_set):
+                            N_curr_set = np.sum(frames_in_this_set)
+
+                            # average subset of frames for set k
+                            psi_subset = psi_chunk[frames_in_this_set]
+                            gU1_subset = -grad_U1[frames_in_this_set]
+                            gradf_subset = grad_f[frames_in_this_set]
+                            testf_subset = test_f[frames_in_this_set]
+                            Lap_f_subset = Lap_f[frames_in_this_set]
+
+                            # dot products with eigenvectors
+                            curr_X1 = np.einsum("tm,tdr,tdp->mpr", psi_subset, gU1_subset, gradf_subset).reshape((M*P, R))
+                            curr_X2 = np.einsum("m,tm,tp->mp", kappa, psi_subset, testf_subset).reshape(M*P)
+
+                            curr_d = (-1./self.beta)*np.einsum("tm,tp->mp", psi_subset, Lap_f_subset).reshape(M*P)
+
+                            if self.using_U0:
+                                gU0_subset = grad_U0[frames_in_this_set]
+                                curr_d += np.einsum("tm,td,tdp->mp", psi_subset, gU0_subset, gradf_subset).reshape(M*P)
+
+                            # running average to reduce numerical error
+                            X[k,:,:-1] = (curr_X1 + N_prev_set[k]*X[k,:,:-1])/(N_prev_set[k] + N_curr_set)
+                            X[k,:,-1] = (curr_X2 + N_prev_set[k]*X[k,:,-1])/(N_prev_set[k] + N_curr_set)
+                            d[k,:] = (curr_d + N_prev_set[k]*d[k,:])/(N_prev_set[k] + N_curr_set)
+
+                            N_prev_set[k] += N_curr_set
+                start_idx += N_chunk
+
+        self.cross_val_sets = n_sets
+        self.cross_val_set_n_frames = N_prev_set
+        self.eigenpair_X = X
+        self.eigenpair_d = d
+        self.eigenpair_D2 = D2
+
+    def _eigenpair_Jacobian(self, trajnames, topfile, psinames, ti_file, M=1, cv_names=[]):
+        """Calculate eigenpair matrices
+       
+        Parameters
+        ----------
+        trajnames : list, str
+            Trajectory filenames
+
+        topfile : str
+            Filename for topology (pdb)
+
+        psinames : list, str
+            Filenames for 
+            
+        ti_file : str
+            Filename for timescales
+
+        M : int (default=1)
+            Number of timescales to keep in eigenfunction expansion.
+        
+        cv_names : list, str (opt)
+            Collective variable rilenames if pre-calculated. Will calculate
+            collective variables on the fly if not given. 
+
+        """
+        # TODO: allow collective variable to be different than eigenvector
+
+        JTJ = np.zeros((self.n_cv_dim, self.n_cv_dim), float)
+
+        if self.using_cv and not self.cv_defined:
+            raise ValueError("Collective variables are not defined!")
+
+        print("calculating eigenpair Jacobian...")
+        N_prev = 0
+        for n in range(len(trajnames)):
+            print("  traj: " + str(n+1))
+            sys.stdout.flush()
+
+            start_idx = 0
+            for chunk in md.iterload(trajnames[n], top=topfile, chunk=1000):
+                N_curr = chunk.n_frames
+
+                # cartesian coordinates unraveled
+                xyz_traj = np.reshape(chunk.xyz, (N_curr, self.n_dof))
+
+                Jac = self._cv_cartesian_Jacobian(xyz_traj)
+                curr_JTJ = np.einsum("tmd,tnd->tmn", Jac, Jac)
+                
+                jmin, jmax = curr_JTJ.max()
+
                 # running average to reduce numerical error
-                X[:,:-1] = (curr_X1 + N_prev*X[:,:-1])/(N_prev + N_curr)
-                X[:,-1] = (curr_X2 + N_prev*X[:,-1])/(N_prev + N_curr)
-                d = (curr_d + N_prev*d)/(N_prev + N_curr)
+                JTJ = (curr_JTJ + N_prev*JTJ)/(N_prev + N_curr)
 
                 start_idx += N_curr
                 N_prev += N_curr
 
-        self.eigenpair_X = X
-        self.eigenpair_d = d
-        self.eigenpair_D2 = D2
+        self.eigenpair_JTJ = JTJ
 
 def polymer_library(n_beads, bonds=True, angles=True, non_bond_wca=True, non_bond_gaussians=True):
     # Soon to be deprecated oct 2018
