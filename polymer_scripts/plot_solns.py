9b9d2366b51c885cd64a363dfe6682705560ae8e
Alexander Kluber <ajkluber@rice.edu>
2019-02-04 16:07:52 -0600

    refactored

--- polymer_scripts/plot_solns.py
+++ polymer_scripts/plot_solns.py
@@ -1,12 +1,29 @@
-from __future__ import print_function
+from __future__ import print_function, absolute_import
+import argparse
 import numpy as np
 import matplotlib as mpl
 mpl.use("Agg")
+mpl.rcParams['mathtext.fontset'] = 'cm'
+mpl.rcParams['mathtext.rm'] = 'serif'
 import matplotlib.pyplot as plt
 
 import implicit_force_field.polymer_scripts.util as util
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("name", type=str)
+    parser.add_argument("msm_savedir", type=str)
+    parser.add_argument("--keep_dims", type=int, default=5)
+    parser.add_argument("--plot_d", action="store_true")
+    parser.add_argument("--plot_U", action="store_true")
+    args = parser.parse_args()
+
+    name = args.name
+    msm_savedir = args.msm_savedir
+    keep_dims = args.keep_dims
+    plot_d = args.plot_d
+    plot_U = args.plot_U
+
     M = 1
     n_beads = 25
     name = "c" + str(n_beads)
@@ -20,11 +37,6 @@ if __name__ == "__main__":
 
     using_cv = True
     using_cv_r0 = False
-    #n_cv_basis_funcs = 40
-    #n_cv_test_funcs = 100
-    #n_cv_basis_funcs = 100
-    #n_cv_test_funcs = 200
-
     using_D2 = False
     n_cross_val_sets = 5
 
@@ -34,24 +46,58 @@ if __name__ == "__main__":
         cg_savedir += "_fixed_bonds_angles"
     cg_savedir += "_CV_" + str(M)
 
-    n_basis_test = [[40, 100], [100, 100], [100, 200]]
-    plt.figure()
-    for n_basis, n_test in n_basis_test:
-        print("  {} {}".format(n_basis, n_test))
-        savedir = cg_savedir + "_{}_{}_crossval_{}".format(n_basis, n_test, n_cross_val_sets)
-        coeff = np.load(savedir + "/rdg_cstar.npy")
+    if plot_U:
+        n_basis_test = [[40, 40], [40, 100], [100, 100], [100, 200]]
+        #n_basis_test = [[40, 40], [40, 100], [100, 100]]
+        plt.figure()
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+        for n_basis, n_test in n_basis_test:
+            print("  {} {}".format(n_basis, n_test))
+            savedir = cg_savedir + "_{}_{}_crossval_{}".format(n_basis, n_test, n_cross_val_sets)
+            coeff = np.load(savedir + "/rdg_cstar.npy")
 
-        Ucg, _, cv_r0_basis, cv_r0_test = util.create_polymer_Ucg(msm_savedir, n_beads, M, beta, fixed_bonded_terms, using_cv, using_cv_r0, using_D2, n_basis, n_test)
+            D = 1./coeff[-1]
 
-        U = np.zeros(len(cv_r0_basis))
-        for i in range(len(coeff) - 1):
-            U += coeff[i]*Ucg.cv_U_funcs[i](cv_r0_basis[:,0])
-        U -= U.min()
+            Ucg, _, cv_r0_basis, cv_r0_test = util.create_polymer_Ucg(msm_savedir, n_beads, M, beta, fixed_bonded_terms, using_cv, using_cv_r0, using_D2, n_basis, n_test)
 
-        plt.plot(cv_r0_basis, U, label=r"$n_{{basis}} = {}$ $n_{{test}} = {}$".format(n_basis, n_test))
+            U = np.zeros(len(cv_r0_basis))
+            for i in range(len(coeff) - 1):
+                U += coeff[i]*Ucg.cv_U_funcs[i](cv_r0_basis[:,0])
+            U -= U.min()
 
-    plt.legend()
-    plt.xlabel("TIC1")
-    plt.ylabel("CG Potential")
-    plt.savefig("plots/compare_Ucg_ntest_nbasis.pdf")
-    plt.savefig("plots/compare_Ucg_ntest_nbasis.png")
+            #plt.plot(cv_r0_basis, U, label=r"$n_{{basis}} = {}$ $n_{{test}} = {}$".format(n_basis, n_test))
+            ax1.plot(cv_r0_basis, U, label=r"${}$   ${}$".format(n_basis, n_test))
+
+            ax2.plot(cv_r0_basis, D*np.ones(len(cv_r0_basis)))
+
+        ax1.legend(title=r"$n_{basis}$  $n_{test}$", fontsize=12)
+        ax1.set_xlabel("TIC1")
+        ax1.set_ylabel(r"$U_{cg}$ (k$_B$T)")
+
+        ax2.set_xlabel("TIC1")
+        ax2.set_ylabel(r"$D$")
+        fig.savefig("plots/compare_Ucg_ntest_nbasis.pdf")
+        fig.savefig("plots/compare_Ucg_ntest_nbasis.png")
+
+    if plot_d:
+        n_basis_test = [[40, 40], [100, 100], [100, 200]]
+        #n_basis_test = [[40, 40], [100, 100]]
+        mid_bin = np.load(msm_savedir + "/psi1_mid_bin.npy")[1:-1]
+
+        plt.figure()
+        for n_basis, n_test in n_basis_test:
+            print("  {} {}".format(n_basis, n_test))
+            savedir = cg_savedir + "_{}_{}_crossval_{}".format(n_basis, n_test, n_cross_val_sets)
+            coeff = np.load(savedir + "/rdg_cstar.npy")
+            d = np.load(savedir + "/d.npy")
+            X = np.load(savedir + "/X.npy")
+
+            centers = np.linspace(mid_bin.min(), mid_bin.max(), n_test)
+
+            plt.plot(centers, d, 'o', label=r"${}$".format(n_test))
+
+        plt.legend(title=r"$n_{test}$", fontsize=12)
+        plt.xlabel("$f_j$ center along $\psi_1$")
+        plt.ylabel(r"$\langle \psi_1, \nabla U_0 \cdot \nabla f_j - kT\Delta f_j \rangle$")
+        plt.savefig("plots/compare_d_ntest.pdf")
+        plt.savefig("plots/compare_d_ntest.png")
