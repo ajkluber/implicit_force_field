8c46ab83fda7d7c6f7a64a32fe05435206e1ed8e
Alexander Kluber <alexkluber@gmail.com>
2019-02-08 17:21:19 -0600

    refactored

--- spectral_loss.py
+++ spectral_loss.py
@@ -9,9 +9,10 @@ import scipy.linalg
 
 import mdtraj as md
 
-class LinearLoss(object):
+# TODO: nonlinear loss function
 
-    def __init__(self, savedir, n_cv_sets=5, recalc=False):
+class CrossValidatedLoss(object):
+    def __init__(self, savedir, n_cv_sets=5):
         """Creates matrices for minimizing the linear spectral loss equations
         
         Parameters
@@ -22,20 +23,11 @@ class LinearLoss(object):
         n_cv_sets : opt.
             Number of k-fold cross validation sets to use.
             
-        recalc : bool
-            Recalculated 
-
         """
 
+        self.savedir = savedir
         self.n_cv_sets = n_cv_sets
         self.cv_sets_are_assigned = False
-
-        self.matrices_estimated = False
-        self.savedir = savedir
-        self.recalc = recalc
-
-        if self.matrix_files_exist() and not recalc:
-            self.load_matrices()
 
     def assign_crossval_sets(self, topfile, trajnames):
         """Randomly assign frames to training and validation sets
@@ -72,6 +64,31 @@ class LinearLoss(object):
         self.cv_set_assignment = set_assignment
         self.cv_sets_are_assigned = True
 
+class LinearLoss(CrossValidatedLoss):
+
+    def __init__(self, savedir, n_cv_sets=5, recalc=False):
+        """Creates matrices for minimizing the linear spectral loss equations
+        
+        Parameters
+        ----------
+        savedir : str
+            Where matrices should be saved.
+            
+        n_cv_sets : opt.
+            Number of k-fold cross validation sets to use.
+            
+        recalc : bool
+            Recalculated 
+
+        """
+        CrossValidatedLoss.__init__(self, savedir, n_cv_sets=n_cv_sets)
+
+        self.matrices_estimated = False
+        self.recalc = recalc
+
+        if self.matrix_files_exist() and not recalc:
+            self._load_matrices()
+
     def calc_matrices(self, Ucg, topfile, trajnames, psinames, ti_file, M=1, coll_var_names=None, verbose=True):
         """Calculate eigenpair matrices
        
@@ -98,7 +115,7 @@ class LinearLoss(object):
 
         """
 
-        # TODO: get info from Ucg
+        self.Ucg = Ucg
 
         if self.n_cv_sets is None:
             self.n_cv_sets = 1
@@ -261,6 +278,14 @@ class LinearLoss(object):
         self._save_matrices()
         self._training_and_validation_matrices()
 
+    def matrix_files_exist(self):
+        X_files_exist = np.all([ os.path.exists("{}/X_{}.npy".format(self.savedir, i + 1)) for i in range(self.n_cv_sets) ])
+        d_files_exist = np.all([ os.path.exists("{}/d_{}.npy".format(self.savedir, i + 1)) for i in range(self.n_cv_sets) ])
+        set_files_exist = np.all([ os.path.exists("{}/frame_set_{}.npy".format(self.savedir, i + 1)) for i in range(self.n_cv_sets) ])
+        files_exist = X_files_exist and d_files_exist and set_files_exist
+
+        return files_exist
+
     def _save_matrices(self): 
         for k in range(self.n_cv_sets):
             np.save("{}/X_{}.npy".format(self.savedir, k + 1), self.X_sets[k])
@@ -270,15 +295,7 @@ class LinearLoss(object):
         np.save("{}/X.npy".format(self.savedir), self.X)
         np.save("{}/d.npy".format(self.savedir), self.d)
 
-    def matrix_files_exist(self):
-        X_files_exist = np.all([ os.path.exists("{}/X_{}.npy".format(self.savedir, i + 1)) for i in range(self.n_cv_sets) ])
-        d_files_exist = np.all([ os.path.exists("{}/d_{}.npy".format(self.savedir, i + 1)) for i in range(self.n_cv_sets) ])
-        set_files_exist = np.all([ os.path.exists("{}/frame_set_{}.npy".format(self.savedir, i + 1)) for i in range(self.n_cv_sets) ])
-        files_exist = X_files_exist and d_files_exist and set_files_exist
-
-        return files_exist
-
-    def load_matrices(self):
+    def _load_matrices(self):
         
         if self.n_cv_sets is None:
             raise ValueErro("Need to define number of cross val sets in order to load them")
@@ -379,8 +396,8 @@ class LinearLoss(object):
             train_mse.append([np.mean(train_mse_folds), np.std(train_mse_folds)/np.sqrt(float(self.n_cv_sets))])
             valid_mse.append([np.mean(valid_mse_folds), np.std(valid_mse_folds)/np.sqrt(float(self.n_cv_sets))])
 
-            X_reg = np.dot(X.T, X) + alphas[i]*D
-            d_reg = np.dot(X.T, d)
+            X_reg = np.dot(self.X.T, self.X) + alphas[i]*D
+            d_reg = np.dot(self.X.T, self.d)
             coeffs.append(scipy.linalg.lstsq(X_reg, d_reg, cond=1e-10)[0])
 
         self.alphas = alphas
@@ -388,181 +405,3 @@ class LinearLoss(object):
         self.train_mse = np.array(train_mse)
         self.valid_mse = np.array(valid_mse)
 
-    # TODO: nonlinear loss function
-
-#class NonlinearLoss(object):
-#    def __init__(self):
-#        pass
-#
-#    def nonlinear_spectral_loss(self, coeff, alpha, trajnames, topfile, psinames, ti_file, M=1, cv_names=[], verbose=False, set_assignment=None):
-#        """Calculate eigenpair matrices
-#       
-#        Parameters
-#        ----------
-#        coeff: np.ndarray
-#            Trajectory filenames
-#
-#        trajnames : list, str
-#            Trajectory filenames
-#
-#        topfile : str
-#            Filename for topology (pdb)
-#
-#        psinames : list, str
-#            Filenames for 
-#            
-#        ti_file : str
-#            Filename for timescales
-#
-#        M : int (default=1)
-#            Number of timescales to keep in eigenfunction expansion.
-#        
-#        cv_names : list, str (opt)
-#            Collective variable rilenames if pre-calculated. Will calculate
-#            collective variables on the fly if not given. 
-#
-#        """
-#        # TODO: allow collective variable to be different than eigenvector
-#
-#        raise NotImplementedError
-#
-#        if not set_assignment is None:
-#            n_sets = np.max([ np.max(x) for x in set_assignment]) + 1
-#        else:
-#            n_sets = 1
-#
-#        R = self.n_params
-#        P = self.n_test_funcs
-#
-#        n_U_params = self.n_params 
-#
-#        U_coeff = coeff[:n_U_params]
-#        a_coeff = coeff[n_U_params:]
-#
-#        training_loss = 0
-#        validation_loss = 0
-#
-#        if self.using_cv and not self.cv_defined:
-#            raise ValueError("Collective variables are not defined!")
-#
-#        if len(psinames) != len(trajnames):
-#            raise ValueError("Need eigenvector for every trajectory!")
-#
-#        kappa = 1./np.load(ti_file)[:M]
-#
-#        N_prev = 0
-#        N_prev_set = np.zeros(n_sets, int)
-#        for n in range(len(trajnames)):
-#            if verbose:
-#                if n == len(trajnames) - 1:
-#                    print("eigenpair matrix from traj: {:>5d}/{:<5d} DONE".format(n + 1, len(trajnames)))
-#                else:
-#                    print("eigenpair matrix from traj: {:>5d}/{:<5d}".format(n + 1, len(trajnames)), end="\r")
-#                sys.stdout.flush()
-#            # load eigenvectors
-#            psi_traj = np.array([ np.load(temp_psiname) for temp_psiname in psinames[n] ]).T
-#
-#            if len(cv_names) > 0:
-#                cv_traj = np.array([ np.load(temp_cvname) for temp_cvname in cv_names[n] ]).T
-#            else:
-#                cv_traj = None
-#
-#            start_idx = 0
-#            for chunk in md.iterload(trajnames[n], top=topfile, chunk=1000):
-#                N_chunk = chunk.n_frames
-#
-#                psi_chunk = psi_traj[start_idx:start_idx + N_chunk,:]
-#
-#                if cv_traj is None:
-#                    cv_chunk = self.calculate_cv(chunk)
-#                else:
-#                    cv_chunk = cv_traj[start_idx:start_idx + N_chunk,:]
-#
-#                # cartesian coordinates unraveled
-#                xyz_traj = np.reshape(chunk.xyz, (N_chunk, self.n_dof))
-#
-#                # calculate gradient of fixed and parametric potential terms
-#                grad_U1 = self.gradient_U1(xyz_traj, cv_chunk)
-#
-#
-#                # calculate test function values, gradient, and Laplacian
-#                test_f = self.test_functions(xyz_traj, cv_chunk)
-#                grad_f, Lap_f = self.test_funcs_gradient_and_laplacian(xyz_traj, cv_chunk)
-#
-#                Jac = self._cv_cartesian_Jacobian(xyz_traj) # tmd
-#
-#                #
-#                noise_a_k = self.calculate_noise_basis(xyz_traj, cv_chunk)
-#                grad_a_k = self.gradient_noise(xyz_traj, cv_chunk)
-#
-#                noise_a = np.einsum("k,tk->t", a_coeff, noise_a_k)
-#                grad_a = np.einsum("k,tk->t", a_coeff, grad_a_k)
-#
-#                # calculate b vector
-#                Force = np.einsum("r,tdr->td", U_coeff, grad_U1)
-#
-#                #drift_1 = np.einsum("t,td->t", noise_a, Force)
-#                #drift_2 = np.einsum("", (1./self.beta)*grad_a)
-#
-#
-#                if self.using_U0:
-#                    grad_U0 = self.gradient_U0(xyz_traj, cv_chunk)
-#                    Force += grad_U0
-#
-#                if self.using_D2:
-#                    #TODO
-#                    pass
-#
-#                if n_sets == 1: 
-#                    # dot products with eigenvectors
-#                    curr_X1 = np.einsum("tm,tdr,tdp->mpr", psi_chunk, -grad_U1, grad_f).reshape((M*P, R))
-#                    curr_X2 = np.einsum("m,tm,tp->mp", kappa, psi_chunk, test_f).reshape(M*P)
-#
-#                    curr_d = (-1./self.beta)*np.einsum("tm,tp->mp", psi_chunk, Lap_f).reshape(M*P)
-#
-#                    if self.using_U0:
-#                        curr_d += np.einsum("tm,td,tdp->mp", psi_chunk, grad_U0, grad_f).reshape(M*P)
-#
-#                    # running average to reduce numerical error
-#                    X[0,:,:-1] = (curr_X1 + float(N_prev)*X[0,:,:-1])/(float(N_prev) + float(N_chunk))
-#                    X[0,:,-1] = (curr_X2 + float(N_prev)*X[0,:,-1])/(float(N_prev) + float(N_chunk))
-#                    d[0,:] = (curr_d + float(N_prev)*d[0,:])/(float(N_prev) + float(N_chunk))
-#
-#                    N_prev += N_chunk
-#                else:
-#                    for k in range(n_sets):   
-#                        frames_in_this_set = set_assignment[n][start_idx:start_idx + N_chunk] == k
-#
-#                        if np.any(frames_in_this_set):
-#                            N_curr_set = np.sum(frames_in_this_set)
-#
-#                            # average subset of frames for set k
-#                            psi_subset = psi_chunk[frames_in_this_set]
-#                            gU1_subset = -grad_U1[frames_in_this_set]
-#                            gradf_subset = grad_f[frames_in_this_set]
-#                            testf_subset = test_f[frames_in_this_set]
-#                            Lap_f_subset = Lap_f[frames_in_this_set]
-#
-#                            # dot products with eigenvectors
-#                            curr_X1 = np.einsum("tm,tdr,tdp->mpr", psi_subset, gU1_subset, gradf_subset).reshape((M*P, R))
-#                            curr_X2 = np.einsum("m,tm,tp->mp", kappa, psi_subset, testf_subset).reshape(M*P)
-#
-#                            curr_d = (-1./self.beta)*np.einsum("tm,tp->mp", psi_subset, Lap_f_subset).reshape(M*P)
-#
-#                            if self.using_U0:
-#                                gU0_subset = grad_U0[frames_in_this_set]
-#                                curr_d += np.einsum("tm,td,tdp->mp", psi_subset, gU0_subset, gradf_subset).reshape(M*P)
-#
-#                            # running average to reduce numerical error
-#                            X[k,:,:-1] = (curr_X1 + float(N_prev_set[k])*X[k,:,:-1])/(float(N_prev_set[k] + N_curr_set))
-#                            X[k,:,-1] = (curr_X2 + float(N_prev_set[k])*X[k,:,-1])/(float(N_prev_set[k] + N_curr_set))
-#                            d[k,:] = (curr_d + float(N_prev_set[k])*d[k,:])/(float(N_prev_set[k] + N_curr_set))
-#
-#                            N_prev_set[k] += N_curr_set
-#                start_idx += N_chunk
-#
-#        self.cross_val_sets = n_sets
-#        self.cross_val_set_n_frames = N_prev_set
-#        self.eigenpair_X = X
-#        self.eigenpair_d = d
-#        self.eigenpair_D2 = D2
