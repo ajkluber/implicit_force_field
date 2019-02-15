6cf329fdef4f863674cedaa4e97757a1713fe235
Alexander Kluber <ajkluber@rice.edu>
2019-02-15 12:16:29 -0600

    plot force matching solutions

--- polymer_scripts/plot_solns.py
+++ polymer_scripts/plot_solns.py
@@ -1,4 +1,5 @@
 from __future__ import print_function, absolute_import
+import os
 import argparse
 import numpy as np
 import matplotlib as mpl
@@ -41,10 +42,13 @@ if __name__ == "__main__":
     n_cross_val_sets = 5
 
     msm_savedir = "msm_dists"
-    cg_savedir = "Ucg_eigenpair"
+    eg_savedir = "Ucg_eigenpair"
+    fm_savedir = "Ucg_FM"
     if fixed_bonded_terms: 
-        cg_savedir += "_fixed_bonds_angles"
-    cg_savedir += "_CV_" + str(M)
+        eg_savedir += "_fixed_bonds_angles"
+        fm_savedir += "_fixed_bonds_angles"
+    eg_savedir += "_CV_" + str(M)
+    fm_savedir += "_CV_" + str(M)
 
     if plot_U:
         n_basis_test = [[40, 40], [40, 100], [100, 100], [100, 200]]
@@ -53,22 +57,34 @@ if __name__ == "__main__":
         fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
         for n_basis, n_test in n_basis_test:
             print("  {} {}".format(n_basis, n_test))
-            savedir = cg_savedir + "_{}_{}_crossval_{}".format(n_basis, n_test, n_cross_val_sets)
-            coeff = np.load(savedir + "/rdg_cstar.npy")
+            temp_eg_savedir = eg_savedir + "_{}_{}_crossval_{}".format(n_basis, n_test, n_cross_val_sets)
 
-            D = 1./coeff[-1]
+            eg_coeff = np.load(temp_eg_savedir + "/rdg_cstar.npy")
+
+            D = 1./eg_coeff[-1]
 
             Ucg, _, cv_r0_basis, cv_r0_test = util.create_polymer_Ucg(msm_savedir, n_beads, M, beta, fixed_bonded_terms, using_cv, using_cv_r0, using_D2, n_basis, n_test)
 
             U = np.zeros(len(cv_r0_basis))
-            for i in range(len(coeff) - 1):
-                U += coeff[i]*Ucg.cv_U_funcs[i](cv_r0_basis[:,0])
+            for i in range(len(eg_coeff) - 1):
+                U += eg_coeff[i]*Ucg.cv_U_funcs[i](cv_r0_basis[:,0])
             U -= U.min()
 
             #plt.plot(cv_r0_basis, U, label=r"$n_{{basis}} = {}$ $n_{{test}} = {}$".format(n_basis, n_test))
-            ax1.plot(cv_r0_basis, U, label=r"${}$   ${}$".format(n_basis, n_test))
+            ln1 = ax1.plot(cv_r0_basis, U, label=r"${}$   ${}$".format(n_basis, n_test))
 
             ax2.plot(cv_r0_basis, D*np.ones(len(cv_r0_basis)))
+
+
+            temp_fm_savedir = fm_savedir + "_{}_{}_crossval_{}".format(n_basis, n_test, n_cross_val_sets)
+            if os.path.exists(temp_fm_savedir + "/rdg_cstar.npy"):
+                fm_coeff = np.load(temp_fm_savedir + "/rdg_cstar.npy")
+
+                U_fm = np.zeros(len(cv_r0_basis))
+                for i in range(len(fm_coeff)):
+                    U_fm += fm_coeff[i]*Ucg.cv_U_funcs[i](cv_r0_basis[:,0])
+                U_fm -= U_fm.min()
+                ax1.plot(cv_r0_basis, U_fm, ls="--", color=ln1[0].get_color(), label=r"${}$ FM".format(n_basis))
 
         ax1.legend(title=r"$n_{basis}$  $n_{test}$", fontsize=12)
         ax1.set_xlabel("TIC1")
@@ -87,10 +103,10 @@ if __name__ == "__main__":
         plt.figure()
         for n_basis, n_test in n_basis_test:
             print("  {} {}".format(n_basis, n_test))
-            savedir = cg_savedir + "_{}_{}_crossval_{}".format(n_basis, n_test, n_cross_val_sets)
-            coeff = np.load(savedir + "/rdg_cstar.npy")
-            d = np.load(savedir + "/d.npy")
-            X = np.load(savedir + "/X.npy")
+            temp_eg_savedir = eg_savedir + "_{}_{}_crossval_{}".format(n_basis, n_test, n_cross_val_sets)
+            coeff = np.load(temp_eg_savedir + "/rdg_cstar.npy")
+            d = np.load(temp_eg_savedir + "/d.npy")
+            X = np.load(temp_eg_savedir + "/X.npy")
 
             centers = np.linspace(mid_bin.min(), mid_bin.max(), n_test)
 
