3ea61968b925e8a747121a8d77a4ef35eebd9289
Alexander Kluber <ajkluber@rice.edu>
2019-02-18 15:32:56 -0600

    simplified directory name where cg model is saved

--- eigenpair_soln.py
+++ eigenpair_soln.py
@@ -15,7 +15,7 @@ import simulation.openmm as sop
 
 import implicit_force_field as iff
 import implicit_force_field.polymer_scripts.util as util
-import implicit_force_field.loss_functions as spl
+import implicit_force_field.loss_functions as loss
 
 
 def plot_Ucg_vs_psi1(coeff, Ucg, cv_r0, prefix):
@@ -194,21 +194,30 @@ if __name__ == "__main__":
     parser.add_argument("msm_savedir", type=str)
     parser.add_argument("--psi_dims", type=int, default=1)
     parser.add_argument("--a_coeff", type=float, default=None)
+    parser.add_argument("--using_cv", action="store_true")
     parser.add_argument("--n_basis", type=int, default=40)
     parser.add_argument("--n_test", type=int, default=100)
-    parser.add_argument("--fixed_bonds", action="store_true")
+    parser.add_argument("--n_pair_gauss", type=int, default=10)
+    parser.add_argument("--bond_cutoff", type=int, default=3)
+    parser.add_argument("--fix_back", action="store_true")
+    parser.add_argument("--fix_exvol", action="store_true")
     parser.add_argument("--recalc_matrices", action="store_true")
     args = parser.parse_args()
 
     msm_savedir = args.msm_savedir
     M = args.psi_dims
     a_coeff = args.a_coeff
+    using_cv = args.using_cv
     n_cv_basis_funcs = args.n_basis
     n_cv_test_funcs = args.n_test
-    fixed_bonded_terms = args.fixed_bonds
+    n_pair_gauss = args.n_pair_gauss
+    bond_cutoff = args.bond_cutoff
+    fix_back = args.fix_back
+    fix_exvol = args.fix_exvol
     recalc_matrices = args.recalc_matrices
+    using_U0 = fix_back or fix_exvol
 
-    #python ~/code/implicit_force_field/eigenpair_soln.py msm_dists --psi_dims 1 --n_basis 40 --n_test 100 --fixed_bonds
+    #python ~/code/implicit_force_field/eigenpair_soln.py msm_dists --psi_dims 1 --n_basis 40 --n_test 100 --fix_back
 
     # If simulation were done with OpenMM gamma = 1 ps^-1 
     # then noise coefficient a_coeff = 1ps/mass_ply
@@ -219,21 +228,6 @@ if __name__ == "__main__":
     T = 300
     kb = 0.0083145
     beta = 1./(kb*T)
-    n_pair_gauss = 10
-    M = 1   # number of eigenvectors to use
-    #fixed_bonded_terms = False
-    #fixed_bonded_terms = True
-
-    using_cv = True
-    using_cv_r0 = False
-    #n_cv_basis_funcs = 40
-    #n_cv_test_funcs = 100
-    #n_cv_basis_funcs = 40
-    #n_cv_test_funcs = 40
-    #n_cv_basis_funcs = 100
-    #n_cv_test_funcs = 100
-    #n_cv_basis_funcs = 100
-    #n_cv_test_funcs = 200
 
     if a_coeff is None:
         fixed_a = False
@@ -246,11 +240,18 @@ if __name__ == "__main__":
     #msm_savedir = "msm_dih_dists"
     #msm_savedir = "msm_dists"
 
+    cg_savedir = util.Ucg_dirname("eigenpair", M, using_U0, fix_back, fix_exvol,
+            bond_cutoff, using_cv, n_cv_basis_funcs=n_cv_basis_funcs,
+            n_cv_test_funcs=n_cv_test_funcs, a_coeff=a_coeff)
+
+    print(cg_savedir)
+    raise SystemExit
+
     # create potential energy function
-    Ucg, cg_savedir, cv_r0_basis, cv_r0_test = util.create_polymer_Ucg(
-            msm_savedir, n_beads, M, beta, fixed_bonded_terms, using_cv,
-            using_cv_r0, using_D2, n_cv_basis_funcs, n_cv_test_funcs, 
-            a_coeff=a_coeff, cg_savedir="Ucg_eigenpair")
+    Ucg, cv_r0_basis, cv_r0_test = util.create_polymer_Ucg(
+            msm_savedir, n_beads, M, beta, fix_back, fix_exvol, using_cv,
+            using_D2, n_cv_basis_funcs, n_cv_test_funcs, n_pair_gauss,
+            bond_cutoff, a_coeff=a_coeff)
 
     topfile = glob.glob("run_*/" + name + "_min_cent.pdb")[0]
     trajnames = glob.glob("run_*/" + name + "_traj_cent_*.dcd") 
@@ -265,10 +266,6 @@ if __name__ == "__main__":
             temp_names.append(msm_savedir + "/run_{}_{}_TIC_{}.npy".format(idx1, idx2, n+1))
         psinames.append(temp_names)
 
-    cg_savedir = cg_savedir + "_crossval_{}".format(n_cross_val_sets)
-    if not a_coeff is None:
-        cg_savedir += "_fixed_a"
-
     if not os.path.exists(cg_savedir):
         os.mkdir(cg_savedir)
     print(cg_savedir)
@@ -276,7 +273,7 @@ if __name__ == "__main__":
     ##################################################################
     # calculate matrix X and d 
     ##################################################################
-    s_loss = spl.LinearSpectralLoss(topfile, trajnames, cg_savedir, n_cv_sets=n_cross_val_sets, recalc=recalc_matrices)
+    s_loss = loss.LinearSpectralLoss(topfile, trajnames, cg_savedir, n_cv_sets=n_cross_val_sets, recalc=recalc_matrices)
 
     if not s_loss.matrix_files_exist() or recalc_matrices:
         s_loss.assign_crossval_sets()
--- polymer_scripts/util.py
+++ polymer_scripts/util.py
@@ -7,8 +7,42 @@ import simtk.openmm.app as app
 import simulation.openmm as sop
 import implicit_force_field as iff
 
-def create_polymer_Ucg(msm_savedir, n_beads, M, beta, fixed_bonded_terms, using_cv, using_cv_r0, using_D2, n_cv_basis_funcs, n_cv_test_funcs, a_coeff=None, cg_savedir="Ucg_eigenpair"):
+def Ucg_dirname(method, M, using_U0, fix_back, fix_exvol, bond_cutoff,
+        using_cv, n_cv_basis_funcs=None, n_cv_test_funcs=None,
+        n_pair_gauss=None, a_coeff=None):
 
+    if method == "force matching":
+        cg_savedir = "Ucg_FM"
+    elif method == "eigenpair":
+        cg_savedir = "Ucg_EG"
+    else:
+        raise ValueError("Method must be force matching or eigenpair")
+
+    if using_U0:
+        if fix_back:
+            cg_savedir += "_fixback"
+
+        if fix_exvol:
+            cg_savedir += "_fixexvol"
+    else:
+        cg_savedir += "_noU0"
+
+    if using_cv:
+        cg_savedir += "_CV_{}_{}_{}".format(M, n_cv_basis_funcs, n_cv_test_funcs)
+    else:
+        cg_savedir += "_pair_{}".format(n_pair_gauss)
+
+    if using_U0 or not using_cv:
+        cg_savedir += "_bondcut_{}".format(bond_cutoff)
+
+    if not a_coeff is None:
+        cg_savedir += "_fixed_a"
+
+    return cg_savedir 
+
+def create_polymer_Ucg(msm_savedir, n_beads, M, beta, fix_back, fix_exvol,
+        using_cv, using_D2, n_cv_basis_funcs, n_cv_test_funcs, n_pair_gauss,
+        bond_cutoff, a_coeff=None):
 
     sigma_ply, eps_ply, mass_ply, bonded_params = sop.build_ff.toy_polymer_params()
     r0, kb, theta0, ka = bonded_params
@@ -28,30 +62,29 @@ def create_polymer_Ucg(msm_savedir, n_beads, M, beta, fixed_bonded_terms, using_
     print("creating Ucg...")
     # coarse-grain polymer potential with free parameters
     Ucg = iff.basis_library.PolymerModel(n_beads, beta, using_cv=using_cv, using_D2=using_D2, a_coeff=a_coeff)
-    #cg_savedir = "Ucg_eigenpair"
 
-    if fixed_bonded_terms:
-        cg_savedir += "_fixed_bonds_angles"
+    if fix_back:
         Ucg.harmonic_bond_potentials(r0_nm, scale_factor=kb_kj, fixed=True)
         Ucg.harmonic_angle_potentials(theta0_rad, scale_factor=ka_kj, fixed=True)
         #Ucg.LJ6_potentials(sigma_ply_nm, scale_factor=eps_ply_kj)
-        Ucg.inverse_r12_potentials(sigma_ply_nm, scale_factor=0.5, fixed=True)
+
+    if fix_exvol:
+        Ucg.inverse_r12_potentials(sigma_ply_nm, scale_factor=0.5, fixed=True,
+                bond_cutoff=bond_cutoff)
+    else:
+        Ucg.inverse_r12_potentials(sigma_ply_nm, scale_factor=0.5, fixed=False,
+                bond_cutoff=bond_cutoff)
 
     if using_cv:
         # centers of test functions in collective variable (CV) space
-        if using_cv_r0:
-            cv_r0 = np.load(msm_savedir + "/psi1_mid_bin.npy")
-            cv_w = np.abs(cv_r0[1] - cv_r0[0])*np.ones(len(cv_r0), float)
-            cv_r0 = cv_r0.reshape((len(cv_r0),1))
-        else:
-            temp_cv_r0 = np.load(msm_savedir + "/psi1_mid_bin.npy")[1:-1]
-            cv_r0 = np.linspace(temp_cv_r0.min(), temp_cv_r0.max(), n_cv_basis_funcs)
-            cv_w_basis = 2*np.abs(cv_r0[1] - cv_r0[0])*np.ones(len(cv_r0), float)
-            cv_r0_basis = cv_r0.reshape((-1, 1))
+        temp_cv_r0 = np.load(msm_savedir + "/psi1_mid_bin.npy")[1:-1]
+        cv_r0 = np.linspace(temp_cv_r0.min(), temp_cv_r0.max(), n_cv_basis_funcs)
+        cv_w_basis = 2*np.abs(cv_r0[1] - cv_r0[0])*np.ones(len(cv_r0), float)
+        cv_r0_basis = cv_r0.reshape((-1, 1))
 
-            cv_r0 = np.linspace(temp_cv_r0.min(), temp_cv_r0.max(), n_cv_test_funcs)
-            cv_w_test = 2*np.abs(cv_r0[1] - cv_r0[0])*np.ones(len(cv_r0), float)
-            cv_r0_test = cv_r0.reshape((-1, 1))
+        cv_r0 = np.linspace(temp_cv_r0.min(), temp_cv_r0.max(), n_cv_test_funcs)
+        cv_w_test = 2*np.abs(cv_r0[1] - cv_r0[0])*np.ones(len(cv_r0), float)
+        cv_r0_test = cv_r0.reshape((-1, 1))
              
         ply_idxs = np.arange(n_beads)
         pair_idxs = []
@@ -67,9 +100,7 @@ def create_polymer_Ucg(msm_savedir, n_beads, M, beta, fixed_bonded_terms, using_
         Ucg.gaussian_cv_test_funcs(cv_r0_test, cv_w_test)
         Ucg.gaussian_cv_potentials(cv_r0_basis, cv_w_basis)
 
-        cg_savedir += "_CV_{}_{}_{}".format(M, n_cv_basis_funcs, n_cv_test_funcs)
     else:
-        cg_savedir += "_gauss_pairs_{}".format(n_pair_gauss)
         gauss_r0_nm = np.linspace(0.3, 1, n_pair_gauss)
         gauss_sigma = gauss_r0_nm[1] - gauss_r0_nm[0]
         gauss_w_nm = gauss_sigma*np.ones(len(gauss_r0_nm))
@@ -78,4 +109,4 @@ def create_polymer_Ucg(msm_savedir, n_beads, M, beta, fixed_bonded_terms, using_
         Ucg.vonMises_angle_test_funcs([theta0_rad], [4])
         Ucg.gaussian_pair_test_funcs(gauss_r0_nm, gauss_w_nm)
 
-    return Ucg, cg_savedir, cv_r0_basis, cv_r0_test
+    return Ucg, cv_r0_basis, cv_r0_test
