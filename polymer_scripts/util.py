78dbdb7ddbdc21d7f2e2460aa03027ae34f48798
Alexander Kluber <ajkluber@rice.edu>
2019-03-06 17:35:17 -0600

    refactored Ucg and cg_dirname functions

--- polymer_scripts/util.py
+++ polymer_scripts/util.py
@@ -8,8 +8,8 @@ import simulation.openmm as sop
 import implicit_force_field as iff
 
 def Ucg_dirname(method, M, using_U0, fix_back, fix_exvol, bond_cutoff,
-        using_cv, n_cv_basis_funcs=None, n_cv_test_funcs=None,
-        n_pair_gauss=None, a_coeff=None):
+        using_cv, cv_lin_pot=None, n_cv_basis_funcs=None, n_cv_test_funcs=None,
+        n_pair_gauss=None, pair_symmetry=None, a_coeff=None):
 
     if method == "force-matching":
         cg_savedir = "Ucg_FM"
@@ -28,9 +28,12 @@ def Ucg_dirname(method, M, using_U0, fix_back, fix_exvol, bond_cutoff,
         cg_savedir += "_noU0"
 
     if using_cv:
-        cg_savedir += "_CV_{}_{}_{}".format(M, n_cv_basis_funcs, n_cv_test_funcs)
+        if (cv_lin_pot is None) or (cv_lin_pot == False):
+            cg_savedir += "_CV_{}_{}_{}".format(M, n_cv_basis_funcs, n_cv_test_funcs)
+        else:
+            cg_savedir += "_CV_lin_{}_{}_{}".format(M, n_cv_basis_funcs, n_cv_test_funcs)
     else:
-        cg_savedir += "_pair_{}".format(n_pair_gauss)
+        cg_savedir += "_{}_pair_{}".format(pair_symmetry, n_pair_gauss)
 
     if using_U0 or not using_cv:
         cg_savedir += "_bondcut_{}".format(bond_cutoff)
@@ -43,7 +46,7 @@ def Ucg_dirname(method, M, using_U0, fix_back, fix_exvol, bond_cutoff,
 
 def create_polymer_Ucg(msm_savedir, n_beads, M, beta, fix_back, fix_exvol,
         using_cv, using_D2, n_cv_basis_funcs, n_cv_test_funcs, n_pair_gauss,
-        bond_cutoff, a_coeff=None, n_cvs=None):
+        bond_cutoff, cv_lin_pot=None, a_coeff=None, n_cvs=None, pair_symmetry=None):
 
     sigma_ply, eps_ply, mass_ply, bonded_params = sop.build_ff.toy_polymer_params()
     r0, kb, theta0, ka = bonded_params
@@ -79,45 +82,56 @@ def create_polymer_Ucg(msm_savedir, n_beads, M, beta, fix_back, fix_exvol,
         Ucg.inverse_r12_potentials(sigma_ply_nm, scale_factor=0.5, fixed=False,
                 bond_cutoff=bond_cutoff)
 
+    temp_cv_r0 = np.load(msm_savedir + "/psi1_mid_bin.npy")[1:-1]
+
+    cv_r0 = np.linspace(temp_cv_r0.min(), temp_cv_r0.max(), n_cv_test_funcs)
+    cv_w_test = 2*np.abs(cv_r0[1] - cv_r0[0])*np.ones(len(cv_r0), float)
+    cv_r0_test = cv_r0.reshape((-1, 1))
+
+    ply_idxs = np.arange(n_beads)
+    pair_idxs = []
+    for i in range(len(ply_idxs) - 1):
+        for j in range(i + 4, len(ply_idxs)):
+            pair_idxs.append([ply_idxs[i], ply_idxs[j]])
+    pair_idxs = np.array(pair_idxs)
+
+    if n_cvs is None:
+        n_cvs = M
+
+    # collective variable test functions. 
+    # TODO: more modular treatment of test functions
+    cv_coeff = np.load(msm_savedir + "/tica_eigenvects.npy")[:,:n_cvs]
+    cv_mean = np.load(msm_savedir + "/tica_mean.npy")
+    Ucg.linear_collective_variables(["dist"], pair_idxs, cv_coeff, cv_mean)
+    Ucg.gaussian_cv_test_funcs(cv_r0_test, cv_w_test)
+
     if using_cv:
         print("  collective variable potentials")
         # centers of test functions in collective variable (CV) space
-        temp_cv_r0 = np.load(msm_savedir + "/psi1_mid_bin.npy")[1:-1]
         cv_r0 = np.linspace(temp_cv_r0.min(), temp_cv_r0.max(), n_cv_basis_funcs)
         cv_w_basis = 2*np.abs(cv_r0[1] - cv_r0[0])*np.ones(len(cv_r0), float)
         cv_r0_basis = cv_r0.reshape((-1, 1))
 
-        cv_r0 = np.linspace(temp_cv_r0.min(), temp_cv_r0.max(), n_cv_test_funcs)
-        cv_w_test = 2*np.abs(cv_r0[1] - cv_r0[0])*np.ones(len(cv_r0), float)
-        cv_r0_test = cv_r0.reshape((-1, 1))
-             
-        ply_idxs = np.arange(n_beads)
-        pair_idxs = []
-        for i in range(len(ply_idxs) - 1):
-            for j in range(i + 4, len(ply_idxs)):
-                pair_idxs.append([ply_idxs[i], ply_idxs[j]])
-        pair_idxs = np.array(pair_idxs)
-
-        if n_cvs is None:
-            n_cvs = M
-
-        cv_coeff = np.load(msm_savedir + "/tica_eigenvects.npy")[:,:n_cvs]
-        cv_mean = np.load(msm_savedir + "/tica_mean.npy")
-
-        Ucg.linear_collective_variables(["dist"], pair_idxs, cv_coeff, cv_mean)
         Ucg.gaussian_cv_test_funcs(cv_r0_test, cv_w_test)
+        if (cv_lin_pot is None) or (cv_lin_pot == False):
+            Ucg.using_linear = False
+        else:
+            Ucg.linear_cv_potential()
+            Ucg.using_linear = True
         Ucg.gaussian_cv_potentials(cv_r0_basis, cv_w_basis)
-
     else:
-        print("  pairwise gaussian potentials")
+        print("  parametric pairwise potentials")
         cv_r0_basis = None
         gauss_r0_nm = np.linspace(0.3, 1, n_pair_gauss)
         gauss_sigma = gauss_r0_nm[1] - gauss_r0_nm[0]
-        gauss_w_nm = gauss_sigma*np.ones(len(gauss_r0_nm))
-        Ucg.gaussian_pair_potentials(gauss_r0_nm, gauss_w_nm, scale_factor=10)
-        Ucg.gaussian_bond_test_funcs([r0_nm], [0.3])
-        Ucg.vonMises_angle_test_funcs([theta0_rad], [4])
-        Ucg.gaussian_pair_test_funcs(gauss_r0_nm, gauss_w_nm)
+        gauss_w_nm = 2*gauss_sigma*np.ones(len(gauss_r0_nm))
+        Ucg.gaussian_pair_potentials(gauss_r0_nm, gauss_w_nm, scale_factor=1,
+                symmetry=pair_symmetry)
+
+        # cartesian test functions, didn't make sense.
+        #Ucg.gaussian_bond_test_funcs([r0_nm], [0.3])
+        #Ucg.vonMises_angle_test_funcs([theta0_rad], [4])
+        #Ucg.gaussian_pair_test_funcs(gauss_r0_nm, gauss_w_nm)
 
     return Ucg, cv_r0_basis, cv_r0_test
 
