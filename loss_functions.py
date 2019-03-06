106589a5c5c7b7959b169862e7a21bdc95a64701
Alexander Kluber <ajkluber@rice.edu>
2019-03-06 16:57:57 -0600

    clean up matrix loading saving. lumping dimensional quantities together

--- loss_functions.py
+++ loss_functions.py
@@ -226,7 +226,6 @@ class CrossValidatedLoss(object):
         if not hasattr(self, "X_train_val"):
             self._training_and_validation_matrices()
 
-
         keep_idxs = np.array([ x for x in range(self.X.shape[1]) if x not in fix_ck_idxs ])
 
         X_train_val = []
@@ -243,9 +242,6 @@ class CrossValidatedLoss(object):
 
         X = np.copy(self.X[:,keep_idxs])
         d = np.copy(self.d - np.einsum("ij,j->i", self.X[:,fix_ck_idxs], fix_ck_vals))
-
-        #import pdb
-        #pdb.set_trace()
 
         if method == "ridge":
             D = np.identity(X.shape[1])
@@ -281,7 +277,7 @@ class LinearSpectralLoss(CrossValidatedLoss):
         if self.matrix_files_exist() and not recalc:
             self._load_matrices()
 
-    def calc_matrices(self, Ucg, psinames, ti_file, M=1, coll_var_names=None, verbose=True):
+    def calc_matrices(self, Ucg, psinames, ti_file, M=1, coll_var_names=None, verbose=True, include_trajs=[]):
         """Calculate eigenpair matrices
        
         Parameters
@@ -338,13 +334,29 @@ class LinearSpectralLoss(CrossValidatedLoss):
 
         kappa = 1./np.load(ti_file)[:M]
 
+        #if Ucg.fixed_a_coeff:
+        #    beta_kappa_const = Ucg.beta*kappa/Ucg.a_coeff
+        #else:
+        #    beta_kappa_const = Ucg.beta*kappa
+
+        if len(include_trajs) > 0:
+            n_trajs = len(include_trajs)
+        else:
+            n_trajs = len(self.trajnames)
+
+        count = 0
         N_prev = np.zeros(self.n_cv_sets, float)
         for n in range(len(self.trajnames)):
+            if n in include_trajs:
+                count += 1
+            else:
+                continue
+
             if verbose:
-                if n == len(self.trajnames) - 1:
-                    print("eigenpair matrix from traj: {:>5d}/{:<5d} DONE".format(n + 1, len(self.trajnames)))
+                if count == n_trajs:
+                    print("eigenpair matrix from traj: {:>5d}/{:<5d} DONE".format(count, n_trajs))
                 else:
-                    print("eigenpair matrix from traj: {:>5d}/{:<5d}".format(n + 1, len(self.trajnames)), end="\r")
+                    print("eigenpair matrix from traj: {:>5d}/{:<5d}".format(count, n_trajs), end="\r")
                 sys.stdout.flush()
 
             # load eigenvectors
@@ -383,6 +395,7 @@ class LinearSpectralLoss(CrossValidatedLoss):
                 if Ucg.fixed_a_coeff:
                     grad_U1 *= Ucg.a_coeff
                     Lap_f *= Ucg.a_coeff
+                    #test_f *= beta_kappa_const
 
                 if Ucg.using_U0:
                     grad_U0 = Ucg.gradient_U0(xyz_traj, cv_chunk)
@@ -468,116 +481,7 @@ class LinearSpectralLoss(CrossValidatedLoss):
         self._save_matrices()
         self._training_and_validation_matrices()
 
-    def matrix_files_exist(self):
-        X_files_exist = np.all([ os.path.exists("{}/X_{}.npy".format(self.savedir, i + 1)) for i in range(self.n_cv_sets) ])
-        d_files_exist = np.all([ os.path.exists("{}/d_{}.npy".format(self.savedir, i + 1)) for i in range(self.n_cv_sets) ])
-        set_files_exist = np.all([ os.path.exists("{}/frame_set_{}.npy".format(self.savedir, i + 1)) for i in range(self.n_cv_sets) ])
-        files_exist = X_files_exist and d_files_exist and set_files_exist
-
-        return files_exist
-
-    def _save_matrices(self): 
-        for k in range(self.n_cv_sets):
-            np.save("{}/X_{}.npy".format(self.savedir, k + 1), self.X_sets[k])
-            np.save("{}/d_{}.npy".format(self.savedir, k + 1), self.d_sets[k])
-            np.save("{}/frame_set_{}.npy".format(self.savedir,  k + 1), self.cv_set_assignment[k])    
-
-        np.save("{}/X.npy".format(self.savedir), self.X)
-        np.save("{}/d.npy".format(self.savedir), self.d)
-
-    def _load_matrices(self):
-        
-        if self.n_cv_sets is None:
-            raise ValueErro("Need to define number of cross val sets in order to load them")
-
-        print("Loaded saved matrices...")
-        self.X_sets = [ np.load("{}/X_{}.npy".format(self.savedir, i + 1)) for i in range(self.n_cv_sets) ]
-        self.d_sets = [ np.load("{}/d_{}.npy".format(self.savedir, i + 1)) for i in range(self.n_cv_sets) ]
-        set_assignment = [ np.load("{}/frame_set_{}.npy".format(self.savedir, i + 1)) for i in range(self.n_cv_sets) ]
-
-        self.n_frames_in_set = []
-        for k in range(self.n_cv_sets):
-            self.n_frames_in_set.append(np.sum([ np.sum(set_assignment[i] == k) for i in range(self.n_cv_sets) ]))
-        self.total_n_frames = np.sum([ set_assignment[i].shape[0] for i in range(self.n_cv_sets) ])
-        self.set_weights = [ (self.n_frames_in_set[j]/float(self.total_n_frames)) for j in range(self.n_cv_sets) ]
-
-        if self.n_cv_sets > 1:
-            self.X = np.sum([ self.set_weights[j]*self.X_sets[j] for j in range(self.n_cv_sets) ], axis=0)
-            self.d = np.sum([ self.set_weights[j]*self.d_sets[j] for j in range(self.n_cv_sets) ], axis=0)
-        else:
-            self.X = self.X_sets[0]
-            self.d = self.d_sets[0]
-
-        self.matrices_estimated = True
-        self.cv_sets_are_assigned = True
-        self._training_and_validation_matrices()
-
-
-    def solve(self, alphas, method="ridge"):
-        """Ridge regression with cross-validation
-        
-        Parameters
-        ----------
-        alphas : np.array
-            Regularization parameter values.
-
-        method : str
-            Regularization method to use.
-            
-        """
-
-        if not self.matrices_estimated:
-            raise ValueError("Need to calculate or eigenpair matrices")
-
-        if not hasattr(self, "X_train_val"):
-            self._training_and_validation_matrices()
-
-        if method == "ridge":
-            D = np.identity(self.X.shape[1])
-        else:
-            raise ValueError("Only method=ridge supported")
-
-        coeffs = [] 
-        train_mse = []
-        valid_mse = []
-        for i in range(len(alphas)):
-            if i == len(alphas) - 1: 
-                print("Solving: {:>5d}/{:<5d} DONE".format(i+1, len(alphas)))
-            else:
-                print("Solving: {:>5d}/{:<5d}".format(i+1, len(alphas)), end="\r")
-            sys.stdout.flush()
-            
-            # folds are precalculated matrices on trajectory chunks
-            train_mse_folds = [] 
-            valid_mse_folds = [] 
-            for k in range(self.n_cv_sets):
-                X_train, X_val = self.X_train_val[k]
-                d_train, d_val = self.d_train_val[k]
-
-                X_reg = np.dot(X_train.T, X_train) + alphas[i]*D
-                d_reg = np.dot(X_train.T, d_train)
-                coeff_fold = scipy.linalg.lstsq(X_reg, d_reg, cond=1e-10)[0]
-        
-                train_mse_folds.append(np.mean((np.dot(X_train, coeff_fold) - d_train)**2))
-                valid_mse_folds.append(np.mean((np.dot(X_val, coeff_fold) - d_val)**2))
-        
-            train_mse.append([np.mean(train_mse_folds), np.std(train_mse_folds)/np.sqrt(float(self.n_cv_sets))])
-            valid_mse.append([np.mean(valid_mse_folds), np.std(valid_mse_folds)/np.sqrt(float(self.n_cv_sets))])
-
-            X_reg = np.dot(self.X.T, self.X) + alphas[i]*D
-            d_reg = np.dot(self.X.T, self.d)
-            coeffs.append(scipy.linalg.lstsq(X_reg, d_reg, cond=1e-10)[0])
-
-        self.alphas = alphas
-        self.coeffs = np.array(coeffs)
-        self.train_mse = np.array(train_mse)
-        self.valid_mse = np.array(valid_mse)
-        
-        self.alpha_star_idx = np.argmin(valid_mse)
-        self.coeff_star = coeffs[self.alpha_star_idx]
-
-
-    def scalar_product_Gen_fj(self, Ucg, coeff, psinames, M=1, cv_names=[]):
+    def scalar_product_Gen_fj(self, Ucg, coeff, psinames, M=1, cv_names=[], include_trajs=[]):
 
         #TODO
 
@@ -604,12 +508,120 @@ class LinearSpectralLoss(CrossValidatedLoss):
         if len(psinames) != len(self.trajnames):
             raise ValueError("Need eigenvector for every trajectory!")
 
+        if len(include_trajs) > 0:
+            n_trajs = len(include_trajs)
+        else:
+            n_trajs = len(self.trajnames)
+        
         N_prev = 0
+        count = 0
         for n in range(len(self.trajnames)):
-            if n == len(self.trajnames) - 1:
-                print("scalar product traj: {:>5d}/{:<5d} DONE".format(n + 1, len(self.trajnames)))
+            if n in include_trajs:
+                count += 1
             else:
-                print("scalar product traj: {:>5d}/{:<5d}".format(n + 1, len(self.trajnames)), end="\r")
+                continue
+
+            if n == len(self.trajnames) - 1:
+                print("scalar product traj: {:>5d}/{:<5d} DONE".format(count, n_trajs))
+            else:
+                print("scalar product traj: {:>5d}/{:<5d}".format(count, n_trajs), end="\r")
+            sys.stdout.flush()
+
+            # load eigenvectors
+            psi_traj = np.array([ np.load(temp_psiname) for temp_psiname in psinames[n] ]).T
+
+            if len(cv_names) > 0:
+                cv_traj = np.array([ np.load(temp_cvname) for temp_cvname in cv_names[n] ]).T
+            else:
+                cv_traj = None
+
+            start_idx = 0
+            for chunk in md.iterload(self.trajnames[n], top=self.topfile, chunk=1000):
+                N_chunk = chunk.n_frames
+
+                psi_chunk = psi_traj[start_idx:start_idx + N_chunk,:]
+
+                if cv_traj is None:
+                    cv_chunk = Ucg.calculate_cv(chunk)
+                else:
+                    cv_chunk = cv_traj[start_idx:start_idx + N_chunk,:]
+
+                # cartesian coordinates unraveled
+                xyz_traj = np.reshape(chunk.xyz, (N_chunk, Ucg.n_dof))
+
+                # calculate gradient of fixed and parametric potential terms
+                grad_U1 = Ucg.gradient_U1(xyz_traj, cv_chunk)
+                grad_U1 = np.einsum("r,tdr->td", c_r, grad_U1)
+
+                # calculate test function values, gradient, and Laplacian
+                test_f = Ucg.test_functions(xyz_traj, cv_chunk)
+                grad_f, Lap_f = Ucg.test_funcs_gradient_and_laplacian(xyz_traj, cv_chunk)
+
+                if Ucg.using_U0:
+                    grad_U0 = Ucg.gradient_U0(xyz_traj, cv_chunk)
+                    curr_psi_gradU0_fj = np.einsum("tm, td,tdp->mp", psi_chunk, -a_coeff*grad_U0, grad_f)
+                    psi_gU0_fj = (curr_psi_gradU0_fj + float(N_prev)*psi_gU0_fj)/(float(N_prev + N_chunk))
+
+                # calculate generator terms
+                curr_psi_fj = np.einsum("tm,tp->mp", psi_chunk, test_f)
+                curr_psi_gradU1_fj = np.einsum("tm,td,tdp->mp", psi_chunk, -a_coeff*grad_U1, grad_f)
+                curr_psi_Lap_fj = np.einsum("tm,tp->mp", psi_chunk, (a_coeff/Ucg.beta)*Lap_f)
+
+                psi_fj = (curr_psi_fj + float(N_prev)*psi_fj)/(float(N_prev + N_chunk))
+                psi_gU1_fj = (curr_psi_gradU1_fj + float(N_prev)*psi_gU1_fj)/(float(N_prev + N_chunk))
+                psi_Lap_fj = (curr_psi_Lap_fj + float(N_prev)*psi_Lap_fj)/(float(N_prev + N_chunk))
+
+                start_idx += N_chunk
+                N_prev += N_chunk
+
+        self.psi_fj = psi_fj
+        self.psi_gU0_fj = psi_gU0_fj
+        self.psi_gU1_fj = psi_gU1_fj
+        self.psi_Lap_fj = psi_Lap_fj
+        self.psi_Gen_fj = psi_gU0_fj + psi_gU1_fj + psi_Lap_fj 
+
+    def scalar_product_grad_psi_grad_fj(self, Ucg, coeff, psinames, M=1, cv_names=[], include_trajs=[]):
+
+        #TODO
+
+        raise NotImplementedError
+
+        if Ucg.fixed_a_coeff:
+            c_r = coeff
+            a_coeff = Ucg.a_coeff
+        else:
+            c_r = coeff[:-1] 
+            a_coeff = 1./coeff[-1]  #?
+
+        P = Ucg.n_test_funcs
+        if Ucg.constant_a_coeff:
+            psi_fj = np.zeros((M, P), float)
+            psi_gU0_fj = np.zeros((M, P), float)
+            psi_gU1_fj = np.zeros((M, P), float)
+            psi_Lap_fj = np.zeros((M, P), float)
+            psi_Gen_fj = np.zeros((M, P), float)
+        else:
+            raise NotImplementedError("Only constant diffusion coefficient is supported.")
+
+        if Ucg.using_cv and not Ucg.cv_defined:
+            raise ValueError("Collective variables are not defined!")
+
+        if len(psinames) != len(self.trajnames):
+            raise ValueError("Need eigenvector for every trajectory!")
+
+        n_trajs = len(self.trajnames) - len(include_trajs)
+        N_prev = 0
+        count = 0
+        for n in range(len(self.trajnames)):
+            if n in include_trajs:
+                count += 1
+            else:
+                continue
+
+            if n == len(self.trajnames) - 1:
+                print("scalar product traj: {:>5d}/{:<5d} DONE".format(count, n_trajs))
+            else:
+                print("scalar product traj: {:>5d}/{:<5d}".format(count, n_trajs), end="\r")
             sys.stdout.flush()
 
             # load eigenvectors
